dc3d76f708aafe8fcf02eec187a8edad04d86505
Dominique Dumont <domi.dumont@free.fr>
2006-03-16 12:30:37 +0000

    - fixed undef warning
    --HG--
    extra : convert_revision : svn%3Aec020cd8-820c-41dc-a724-4838206f7038/trunk%4053

--- config-model-core/WarpedNode.pm
+++ config-model-core/WarpedNode.pm
@@ -1,7 +1,7 @@
 # $Author: ddumont $
-# $Date: 2006-02-23 13:43:30 $
+# $Date: 2006-03-16 12:30:37 $
 # $Name: not supported by cvs2svn $
-# $Revision: 1.2 $
+# $Revision: 1.3 $
 
 #    Copyright (c) 2005,2006 Dominique Dumont.
 #
@@ -32,7 +32,7 @@ use Config::Model::Exception ;
 use Data::Dumper ;
 
 use vars qw($VERSION $AUTOLOAD) ;
-$VERSION = sprintf "%d.%03d", q$Revision: 1.2 $ =~ /(\d+)\.(\d+)/;
+$VERSION = sprintf "%d.%03d", q$Revision: 1.3 $ =~ /(\d+)\.(\d+)/;
 
 =head1 NAME
 
@@ -258,7 +258,8 @@ sub set {
       if ref $config_class_name ;
 
     # check if some action is needed (ie. create or morph node)
-    return if $self->{config_class_name} eq $config_class_name ;
+    return if defined $self->{config_class_name} 
+      and $self->{config_class_name} eq $config_class_name ;
 
     my $old_object = $self->{data} ;
     my $morph = $self->{warp}{morph} ;
