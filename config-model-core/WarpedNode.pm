000829d205fd3f5eb6841998fb3c4e7df745705a
Dominique Dumont <domi.dumont@free.fr>
2007-01-08 12:40:07 +0000

    - bug fix: also forward 'config_model' method to contained node
    --HG--
    extra : convert_revision : svn%3Aec020cd8-820c-41dc-a724-4838206f7038/trunk%40224

--- config-model-core/WarpedNode.pm
+++ config-model-core/WarpedNode.pm
@@ -1,7 +1,7 @@
 # $Author: ddumont $
-# $Date: 2006-07-19 10:31:44 $
+# $Date: 2007-01-08 12:40:07 $
 # $Name: not supported by cvs2svn $
-# $Revision: 1.5 $
+# $Revision: 1.6 $
 
 #    Copyright (c) 2005,2006 Dominique Dumont.
 #
@@ -32,7 +32,7 @@ use Config::Model::Exception ;
 use Data::Dumper ;
 
 use vars qw($VERSION $AUTOLOAD) ;
-$VERSION = sprintf "%d.%03d", q$Revision: 1.5 $ =~ /(\d+)\.(\d+)/;
+$VERSION = sprintf "%d.%03d", q$Revision: 1.6 $ =~ /(\d+)\.(\d+)/;
 
 =head1 NAME
 
@@ -209,7 +209,7 @@ sub name {
 foreach my $method (qw/fetch_element config_class_name get_element_name
                        has_element is_element_available element_type load
 		       fetch_element_value get_type get_cargo_type 
-                       describe/
+                       describe config_model/
 		   ) {
     # to register new methods in package
     no strict "refs"; 
@@ -363,7 +363,7 @@ will be done whenever C<tree_macro> is changed.
 
 =head1 AUTHOR
 
-Dominique Dumont, domi@komarr.grenoble.hp.com
+Dominique Dumont, (ddumont at cpan dot org)
 
 =head1 SEE ALSO
 
