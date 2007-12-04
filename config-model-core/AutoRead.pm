e6ca781753a38d8ed46b796094725066569c3c97
Dominique Dumont <domi.dumont@free.fr>
2007-12-04 12:31:22 +0000

    - read_ini: added capability to read configuration data from ini file   (See Config::Tiny) - write_ini: can use ini files to store configuration data - read_perl: added capability to read configuration data from a perl   data structure (see Config::Model::DumpAsData) from a .pl - write_perl: can use perl file to store configuration data (.pl file) - auto_read_init: configuration model can specify cds (dump string see   Config::Model::Dumper), perl or ini with 'read_config' and   'write_config' parameter.
    --HG--
    extra : convert_revision : svn%3Aec020cd8-820c-41dc-a724-4838206f7038/trunk%40434

--- config-model-core/AutoRead.pm
+++ config-model-core/AutoRead.pm
@@ -1,7 +1,7 @@
 # $Author: ddumont $
-# $Date: 2007-06-06 12:14:51 $
+# $Date: 2007-12-04 12:31:22 $
 # $Name: not supported by cvs2svn $
-# $Revision: 1.10 $
+# $Revision: 1.11 $
 
 #    Copyright (c) 2005-2007 Dominique Dumont.
 #
@@ -26,12 +26,13 @@ use Carp;
 use strict;
 use warnings ;
 use Config::Model::Exception ;
+use Data::Dumper ;
 use File::Path ;
 use UNIVERSAL ;
 
 use base qw/Config::Model::AnyThing/ ;
 
-our $VERSION = sprintf "%d.%03d", q$Revision: 1.10 $ =~ /(\d+)\.(\d+)/;
+our $VERSION = sprintf "%d.%03d", q$Revision: 1.11 $ =~ /(\d+)\.(\d+)/;
 
 =head1 NAME
 
@@ -39,6 +40,7 @@ Config::Model::AutoRead - Load on demand base class for configuration node
 
 =head1 SYNOPSIS
 
+  # top level config file name matches instance name
   $model->create_config_class 
   (
    config_class_name => 'OneAutoReadConfigClass',
@@ -54,20 +56,27 @@ Config::Model::AutoRead - Load on demand base class for configuration node
   # config data will be written in /etc/my_config_dir/foo.cds
   my $instance = $model->instance(instance_name => 'foo') ;
 
+
 =head1 DESCRIPTION
 
-This class provides a way to read on demand the configuration
-informations. 
+This class provides a way to specify how to read or write configuration
+data within the model (instead of writing dedicated perl code).
 
 In other words, when a node object is created, all the configuration
-information are read during creation.
+information are read during creation of the node.
+
+=begin comment
 
 This feature is also useful if you want to read configuration class
 declarations at run time. (For instance in a C</etc> directory like
-C</etc/config_model.d>). In this case, each configuration class must
+C</etc/some_config.d>). In this case, each configuration class must
 specify how to read and write configuration information.
 
-This read/write features can be:
+Idea: sub-files name could be <instance>%<location>.cds
+
+=end comment
+
+This read/write can be done with:
 
 =over
 
@@ -75,6 +84,16 @@ This read/write features can be:
 
 Config dump string (cds). I.e. a string that describes the content of
 a configuration tree. See L<Config::Model::Dumper>.
+
+=item *
+
+Ini files (written with L<Config::Tiny>. See limitations in 
+L</"Limitations depending on storage">.
+
+=item *
+
+Perl data structure (perl). See L<Config::Model::DumpAsData> for
+details on the data structure.
 
 =item *
 
@@ -90,6 +109,44 @@ read and load the configuration tree.
 When read, the object registers itself to the instance. Then the user
 can call the C<write_back> method on the instance (See
 L<Config::Model::Instance>) to write all configuration informations.
+
+=head2 Built-in read write format
+
+Currently, this class supports the following built-in formats:
+
+=over
+
+=item cds
+
+Config dumpt string. See L<Config::Model::Dumper>.
+
+=item ini
+
+Ini files written by L<Config::Tiny>.
+
+=back
+
+=head1 Limitations depending on storage
+
+Some storage system will limit the structure of the model you can map
+to the file.
+
+=head2 Ini files limitation
+
+Structure of the Config::Model must be very simple. Either:
+
+=over
+
+=item *
+
+A single class with hash of leaves elements.
+
+=item * 
+
+2 levels of classes. The top level has nodes elements. All other
+classes have only leaf elements.
+
+=back
 
 =head1 Configuration class with auto read or auto write
 
@@ -109,9 +166,10 @@ The various C<read> method will be tried in order specified:
 =item *
 
 First the cds file name which depend on the parameters used in model
-creation and instance creation:   
+creation and instance creation:
 C<< <model:config_dir>/<instance_name>.cds >>
 The syntax of the C<cds> file is described in  L<Config::Model::Dumper>.
+
 =item * 
 
 A call to C<Bar::read_it> with these parameters:
@@ -126,16 +184,16 @@ be skipped.
 When necessary (or required by the user), all configuration
 informations are written back using B<all> the write method passed.
 
-In the example above, only a C<cds> file is written. The example above
-is an example of a graceful migration from a customized format to a
-C<cds> format.
+In the example above, only a C<cds> file is written. But, both custom
+format and C<cds> file are tried, this example is also an example of a
+graceful migration from a customized format to a C<cds> format.
 
-To read and write only customized files :
+You can choose also to read and write only customized files :
 
   read_config  => { class => 'Bar' ,  function => 'read_it'},
   write_config => { class => 'Bar' ,  function => 'write_it'};
 
-To read and write only cds files :
+Or to read and write only cds files :
 
   read_config  => 'cds', 
   write_config => 'cds' ;
@@ -171,7 +229,7 @@ written in another directory:
 
 =cut
 
-# called at configuration class creation
+# called at configuration node creation
 sub auto_read_init {
     my ($self, $readlist, $r_dir) = @_ ;
 
@@ -185,8 +243,10 @@ sub auto_read_init {
 
     my @list = ref $readlist  eq 'ARRAY' ? @$readlist :  ($readlist) ;
     foreach my $read (@list) {
-	last if ($read eq 'xml' and $self->read_xml()) ;
-	last if ($read eq 'cds' and $self->read_cds()) ;
+	last if ($read eq 'xml'  and $self->read_xml()) ;
+	last if ($read eq 'ini'  and $self->read_ini()) ;
+	last if ($read eq 'perl' and $self->read_perl()) ;
+	last if ($read eq 'cds'  and $self->read_cds()) ;
 	next unless ref($read) eq 'HASH' ;
 
 	my $c = my $file = $read->{class} ;
@@ -198,6 +258,7 @@ sub auto_read_init {
     }
 }
 
+# called at configuration node creation
 sub auto_write_init {
     my ($self, $wrlist, $w_dir) = @_ ;
 
@@ -209,12 +270,24 @@ sub auto_write_init {
     # provide a proper write back function
     my @array = ref $wrlist eq 'ARRAY' ? @$wrlist : ($wrlist) ;
     foreach my $write (@array) {
+	print "auto_write_init: registering write cb ($write) for ",$self->name,"\n"
+	  if $::debug ;
 	my $wb ;
 	if ($write eq 'xml') {
 	    $wb = sub {$self->write_xml() ;} ;
+	    $self->{auto_write}{xml} = 1 ;
+	}
+	elsif ($write eq 'ini') {
+	    $wb = sub {$self->write_ini() ;} ;
+	    $self->{auto_write}{ini} = 1 ;
+	}
+	elsif ($write eq 'perl') {
+	    $wb = sub {$self->write_perl() ;} ;
+	    $self->{auto_write}{perl} = 1 ;
 	}
 	elsif ($write eq 'cds') {
 	    $wb = sub {$self->write_cds() ;} ;
+	    $self->{auto_write}{cds} = 1 ;
 	}
 	elsif (ref($write) eq 'HASH') {
 	    my $c = my $file = $write->{class} ;
@@ -226,10 +299,17 @@ sub auto_write_init {
 			 &{$c.'::'.$f}(conf_dir => $self->{w_dir}, 
 				       object => $safe_self) ;
 		     };
+	    $self->{auto_write}{custom} = 1 ;
 	}
 
 	$instance->register_write_back($wb) ;
     }
+}
+
+sub is_auto_write_for_type {
+    my $self = shift;
+    my $type = shift ;
+    return $self->{auto_write}{$type} || 0;
 }
 
 sub get_cfg_file_name
@@ -246,13 +326,18 @@ sub get_cfg_file_name
     croak "get_cfg_file_name: no read/write directory provided by instance"
       unless defined $dir ;
 
-    $dir .= "/". $i->name ;
     mkpath ($dir,0, 0755) if $create_dir and not -d $dir ;
 
-    my $loc = $self->location ;
-    $dir .= '/' . $loc if $loc ;
+    # TBD should we use sub-directories ?? 
 
-    return $dir ;
+    # append instance name
+    my $name = $dir ."/". $i->name ;
+
+    # append ":foo bar" if not root object
+    my $loc = $self->location ; # not good
+    $name .= ':' . $loc if $loc ;
+
+    return $name ;
   }
 
 sub read_cds
@@ -260,8 +345,11 @@ sub read_cds
     my $self = shift;
     my $file_name = $self->get_cfg_file_name('r') . '.cds' ;
     return 0 unless -r $file_name ;
-    my $text = slurp($file_name) ;
-    walk(object => $self, step => $text) ;
+    open(IN,$file_name) || die "Cannot open $file_name:$!";
+    local $/ ; # slurp mode
+    my $text = <IN> ;
+    close IN ;
+    $self->load( step => $text) ;
     return 1 ;
   }
 
@@ -272,8 +360,83 @@ sub write_cds
     my $i = $self->instance ;
     my $file_name = $self->get_cfg_file_name('w',1) . '.cds' ;
     open (FOUT, ">$file_name") or die "_write_cds: Can't open $file_name: $!";
-    print FOUT $self->dump_tree ;
+    print FOUT $self->dump_tree(skip_auto_write => 1 ) ;
     close FOUT ;
+    return 1 ;
+  }
+
+sub read_perl
+  {
+    my $self = shift;
+    my $file_name = $self->get_cfg_file_name('r') . '.pl' ;
+    return 0 unless -r $file_name ;
+
+    my $pdata = do $file_name || die "Cannot open $file_name:$!";
+    $self->load_data( $pdata ) ;
+    return 1 ;
+  }
+
+sub write_perl
+  {
+    my $self = shift;
+
+    my $i = $self->instance ;
+    my $file_name = $self->get_cfg_file_name('w',1) . '.pl' ;
+
+    my $p_data = $self->dump_as_data(skip_auto_write => 1 ) ;
+
+    my $dumper = Data::Dumper->new([$p_data]) ;
+    $dumper->Terse(1) ;
+
+    open (FOUT, ">$file_name") or die "_write_perl: Can't open $file_name: $!";
+    print FOUT $dumper->Dump , ";\n";
+    close FOUT ;
+
+    return 1 ;
+  }
+
+sub read_ini
+  {
+    my $self = shift;
+    my $file_name = $self->get_cfg_file_name('r') . '.ini' ;
+    return 0 unless -r $file_name ;
+    require Config::Tiny;
+    my $iniconf = Config::Tiny->new();
+    my $conf_data = $iniconf -> read($file_name) ;
+
+    # load root properties
+    if (defined $conf_data->{_}) {
+	my $root_data = delete $conf_data->{_} ;
+	$self->load_data($root_data) ;
+    }
+    $self->load_data($conf_data) ;
+
+    return 1 ;
+  }
+
+sub write_ini
+  {
+    my $self = shift;
+
+    my $i = $self->instance ;
+    my $file_name = $self->get_cfg_file_name('w',1) . '.ini' ;
+
+    require Config::Tiny;
+    my $iniconf = Config::Tiny->new() ;
+
+    my $data = $self->dump_as_data(skip_auto_write => 1 ) ;
+
+    foreach my $k (keys %$data) {
+	if (ref( $data->{$k} )) {
+	    $iniconf->{$k} = $data->{$k} ;
+	}
+	else {
+	    $iniconf->{_}{$k} = $data->{$k} ;
+	}
+    }
+
+    $iniconf -> write($file_name) ;
+
     return 1 ;
   }
 
@@ -292,7 +455,6 @@ sub write_xml
 1;
 
 __END__
-
 
 =head1 AUTHOR
 
