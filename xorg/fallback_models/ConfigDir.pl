d635d775ea9deafc7912434293c82f281eebf673
Dominique Dumont <domi.dumont@free.fr>
2008-01-23 11:10:00 +0000

    - first version
    --HG--
    extra : convert_revision : svn%3Aec020cd8-820c-41dc-a724-4838206f7038/trunk%40445

---
+++ xorg/fallback_models/ConfigDir.pl
@@ -0,0 +1,12 @@
+# Generated file. Do not edit
+
+[
+          [
+            'name',
+            'Xorg::ConfigDir',
+            'generated_by',
+            'Config::Model Build.PL',
+            'config_dir',
+            '/etc/X11'
+          ]
+        ]
---
+++ xorg/fallback_models/Xorg-it.pl
@@ -0,0 +1,14 @@
+# Generated file. Do not edit
+
+[
+          [
+            'name',
+            'Xorg::InputDevice::KeyboardOptModel::Xorg-it',
+            'generated_by',
+            'Config::Model Build.PL',
+            'element',
+            [],
+            'description',
+            []
+          ]
+        ]
