ee002384d3489acb5b4d2adbab15c4bf53429ca3
Dominique Dumont <domi.dumont@free.fr>
2008-03-28 18:06:23 +0000

    modified but broken
    --HG--
    extra : convert_revision : svn%3Aec020cd8-820c-41dc-a724-4838206f7038/trunk%40572

--- config-model-sshd/lib/Config/Model/models/Sshd/MatchElement.pl
+++ config-model-sshd/lib/Config/Model/models/Sshd/MatchElement.pl
@@ -2,9 +2,20 @@
           {
             'name' => 'Sshd::MatchElement',
             'description' => [
+                               'AllowTcpForwarding',
+                               "Specifies whether TCP forwarding is permitted. The default is \x{201c}yes\x{201d}.Note that disabling TCP forwarding does not improve security unless users are also denied shell access, as they can always install their own forwarders.
+",
+                               'ForceCommand',
+                               "Forces the execution of the command specified by ForceCommand, ignoring any command supplied by the client. The command is invoked by using the user\x{2019}s login shell with the -c option. This applies to shell, command, or subsystem execution. It is most useful inside a Match block. The command originally supplied by the client is available in the SSH_ORIGINAL_COMMAND environment variable.
+
+",
                                'GatewayPorts',
                                'Specifies whether remote hosts are allowed to connect to ports forwarded for the client.  By default, sshd(8) binds remote port forwardings to the loopback address.  This prevents other remote hosts from connecting to forwarded ports.  GatewayPorts can be used to specify that sshd should allow remote port forwardings to bind to non-loopback addresses, thus allowing other hosts to connect.
-'
+',
+                               'ForceCommand',
+                               "Forces the execution of the command specified by ForceCommand, ignoring any command supplied by the client. The command is invoked by using the user\x{2019}s login shell with the -c option. This applies to shell, command, or subsystem execution. It is most useful inside a Match block. The command originally supplied by the client is available in the SSH_ORIGINAL_COMMAND environment variable.
+
+"
                              ],
             'element' => [
                            'AllowTcpForwarding',
@@ -13,9 +24,9 @@
                              'built_in' => '1',
                              'type' => 'leaf'
                            },
-                           'Banner',
+                           'ForceCommand',
                            {
-                             'value_type' => 'string',
+                             'value_type' => 'uniline',
                              'type' => 'leaf'
                            },
                            'GatewayPorts',
@@ -36,6 +47,11 @@
                                            'clientspecified',
                                            'no'
                                          ]
+                           },
+                           'ForceCommand',
+                           {
+                             'value_type' => 'uniline',
+                             'type' => 'leaf'
                            }
                          ]
           }
