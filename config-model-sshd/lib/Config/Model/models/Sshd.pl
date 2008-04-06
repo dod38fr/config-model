39701bba9b40371eab75bd6967578adf5f8a7316
Dominique Dumont <domi.dumont@free.fr>
2008-04-06 18:34:18 +0000

    finished Sshs::MatchElement
    --HG--
    extra : convert_revision : svn%3Aec020cd8-820c-41dc-a724-4838206f7038/trunk%40590

--- config-model-sshd/lib/Config/Model/models/Sshd.pl
+++ config-model-sshd/lib/Config/Model/models/Sshd.pl
@@ -1,15 +1,47 @@
 [
           {
+            'level' => [
+                         'AcceptEnv',
+                         'normal',
+                         'AddressFamily',
+                         'normal',
+                         'AuthorizedKeysFile',
+                         'normal'
+                       ],
+            'status' => [
+                          'AcceptEnv',
+                          'standard',
+                          'AddressFamily',
+                          'standard',
+                          'AuthorizedKeysFile',
+                          'standard'
+                        ],
             'name' => 'Sshd',
+            'permission' => [
+                              'AcceptEnv',
+                              'intermediate',
+                              'AddressFamily',
+                              'intermediate',
+                              'AuthorizedKeysFile',
+                              'intermediate'
+                            ],
+            'include' => 'Sshd::MatchElement',
             'description' => [
-                               'AuthorizedKeysFile',
-                               'Specifies the file that contains the public keys that can be used for user authentication. AuthorizedKeysFile may contain tokens of the form %T which are substituted during connection setup.
-',
                                'AcceptEnv',
                                "Specifies what environment variables sent by the client will be copied into the session\x{2019}s environ(7).
-"
+",
+                               'AddressFamily',
+                               '',
+                               'AuthorizedKeysFile',
+                               'Specifies the file that contains the public keys that can be used for user authentication. AuthorizedKeysFile may contain tokens of the form %T which are substituted during connection setup.
+'
                              ],
             'element' => [
+                           'AcceptEnv',
+                           {
+                             'type' => 'list',
+                             'cargo_type' => 'leaf'
+                           },
                            'AddressFamily',
                            {
                              'value_type' => 'enum',
@@ -25,12 +57,6 @@
                            {
                              'value_type' => 'uniline',
                              'type' => 'leaf'
-                           },
-                           'AcceptEnv',
-                           {
-                             'value_type' => 'uniline',
-                             'type' => 'list',
-                             'cargo_type' => 'leaf'
                            }
                          ]
           }
--- config-model-sshd/lib/Config/Model/models/Sshd/MatchElement.pl
+++ config-model-sshd/lib/Config/Model/models/Sshd/MatchElement.pl
@@ -1,6 +1,72 @@
 [
           {
+            'level' => [
+                         'AllowTcpForwarding',
+                         'normal',
+                         'Banner',
+                         'normal',
+                         'ForceCommand',
+                         'normal',
+                         'GatewayPorts',
+                         'normal',
+                         'GSSApiAuthentication',
+                         'normal',
+                         'KbdInteractiveAuthentication',
+                         'normal',
+                         'KerberosAuthentication',
+                         'normal',
+                         'PasswordAuthentication',
+                         'normal',
+                         'PermitOpen',
+                         'normal',
+                         'RhostsRSAAuthentication',
+                         'normal'
+                       ],
+            'status' => [
+                          'AllowTcpForwarding',
+                          'standard',
+                          'Banner',
+                          'standard',
+                          'ForceCommand',
+                          'standard',
+                          'GatewayPorts',
+                          'standard',
+                          'GSSApiAuthentication',
+                          'standard',
+                          'KbdInteractiveAuthentication',
+                          'standard',
+                          'KerberosAuthentication',
+                          'standard',
+                          'PasswordAuthentication',
+                          'standard',
+                          'PermitOpen',
+                          'standard',
+                          'RhostsRSAAuthentication',
+                          'standard'
+                        ],
             'name' => 'Sshd::MatchElement',
+            'permission' => [
+                              'AllowTcpForwarding',
+                              'intermediate',
+                              'Banner',
+                              'intermediate',
+                              'ForceCommand',
+                              'intermediate',
+                              'GatewayPorts',
+                              'intermediate',
+                              'GSSApiAuthentication',
+                              'intermediate',
+                              'KbdInteractiveAuthentication',
+                              'intermediate',
+                              'KerberosAuthentication',
+                              'intermediate',
+                              'PasswordAuthentication',
+                              'intermediate',
+                              'PermitOpen',
+                              'intermediate',
+                              'RhostsRSAAuthentication',
+                              'intermediate'
+                            ],
             'description' => [
                                'AllowTcpForwarding',
                                "Specifies whether TCP forwarding is permitted. The default is \x{201c}yes\x{201d}.Note that disabling TCP forwarding does not improve security unless users are also denied shell access, as they can always install their own forwarders.
@@ -20,7 +86,31 @@
 ',
                                'KbdInteractiveAuthentication',
                                'No doc found in sshd documentation
-'
+',
+                               'KerberosAuthentication',
+                               '',
+                               'PasswordAuthentication',
+                               '',
+                               'PermitOpen',
+                               "Specifies the destinations to which TCP port forwarding is permitted. The forwarding specification must be one of the following forms:
+\"host:port\" or \"IPv4_addr:port\" or \"[IPv6_addr]:port\". An argument of \x{201c}any\x{201d} can be used to remove all restrictions and permit any forwarding requests. By default all port forwarding requests are permitted.
+
+",
+                               'RhostsRSAAuthentication',
+                               "Specifies whether rhosts or /etc/hosts.equiv authentication together with successful RSA host authentication is allowed.  The default is \x{201c}no\x{201d}. This option applies to protocol version 1 only.
+",
+                               'RSAAuthentication',
+                               'Specifies whether pure RSA authentication is allowed. This option applies to protocol version 1 only.
+',
+                               'X11DisplayOffset',
+                               "Specifies the first display number available for sshd(8)\x{2019}s X11 forwarding. This prevents sshd from interfering with real X11 servers.
+",
+                               'X11Forwarding',
+                               'Specifies whether X11 forwarding is permitted. Note that disabling X11 forwarding does not prevent users from forwarding X11 traffic, as users can always install their own forwarders. X11 forwarding is automatically disabled if UseLogin is enabled.
+',
+                               'X11UseLocalhost',
+                               "Specifies whether sshd(8) should bind the X11 forwarding server to the loopback address or to the wildcard address.  By default, sshd binds the forwarding server to the loopback address and sets the hostname part of the DISPLAY environment variable to \x{201c}localhost\x{201d}. This prevents remote hosts from connecting to the proxy display.  However, some older X11 clients may not function with this configuration. X11UseLocalhost may be set to \x{201c}no\x{201d} to specify that the forwarding server should be bound to the wildcard address.
+"
                              ],
             'element' => [
                            'AllowTcpForwarding',
@@ -42,6 +132,7 @@
                            'GatewayPorts',
                            {
                              'value_type' => 'enum',
+                             'built_in' => 'no',
                              'help' => {
                                          'yes' => 'force remote port forwardings to bind to the wildcard address
 ',
@@ -50,7 +141,6 @@
                                          'no' => 'No port forwarding
 '
                                        },
-                             'built_in' => 'no',
                              'type' => 'leaf',
                              'choice' => [
                                            'yes',
@@ -81,6 +171,52 @@
                              'value_type' => 'boolean',
                              'built_in' => '0',
                              'type' => 'leaf'
+                           },
+                           'PermitOpen',
+                           {
+                             'cargo_args' => {
+                                               'value_type' => 'uniline'
+                                             },
+                             'type' => 'list',
+                             'cargo_type' => 'leaf'
+                           },
+                           'RhostsRSAAuthentication',
+                           {
+                             'value_type' => 'boolean',
+                             'built_in' => '0',
+                             'type' => 'leaf'
+                           },
+                           'RSAAuthentication',
+                           {
+                             'value_type' => 'boolean',
+                             'built_in' => '1',
+                             'type' => 'leaf'
+                           },
+                           'X11DisplayOffset',
+                           {
+                             'value_type' => 'integer',
+                             'built_in' => '10',
+                             'type' => 'leaf'
+                           },
+                           'X11Forwarding',
+                           {
+                             'value_type' => 'enum',
+                             'built_in' => 'no',
+                             'type' => 'leaf',
+                             'choice' => [
+                                           'yes',
+                                           'no'
+                                         ]
+                           },
+                           'X11UseLocalhost',
+                           {
+                             'value_type' => 'enum',
+                             'built_in' => 'yes',
+                             'type' => 'leaf',
+                             'choice' => [
+                                           'yes',
+                                           'no'
+                                         ]
                            }
                          ]
           }
