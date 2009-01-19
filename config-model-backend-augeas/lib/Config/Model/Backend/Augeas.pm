e3be22319fd0bbb7d0ab963d20d5fa030fcd7f25
Dominique Dumont <domi.dumont@free.fr>
2009-01-19 12:46:33 +0000

    - v0.102
    --HG--
    extra : convert_revision : svn%3Aec020cd8-820c-41dc-a724-4838206f7038/trunk%40843

--- config-model-backend-augeas/lib/Config/Model/Backend/Augeas.pm
+++ config-model-backend-augeas/lib/Config/Model/Backend/Augeas.pm
@@ -32,7 +32,7 @@ my $has_augeas = 1;
 eval { require Config::Augeas ;} ;
 $has_augeas = 0 if $@ ;
 
-our $VERSION = '0.101';
+our $VERSION = '0.102';
 
 =head1 NAME
 
@@ -709,7 +709,7 @@ Dominique Dumont, E<lt>ddumont at cpan dot org@<gt>
 
 =head1 COPYRIGHT
 
-Copyright (C) 2008 by Dominique Dumont
+Copyright (C) 2008-2009 by Dominique Dumont
 
 =head1 LICENSE
 
