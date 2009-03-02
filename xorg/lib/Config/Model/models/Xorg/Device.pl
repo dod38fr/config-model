50d754e041cc01aed526ae8e5f56247f3c83717d
Dominique Dumont <domi.dumont@free.fr>
2009-03-02 13:08:36 +0000

    added Fglrx model
    --HG--
    extra : convert_revision : svn%3Aec020cd8-820c-41dc-a724-4838206f7038/trunk%40866

--- xorg/lib/Config/Model/models/Xorg/Device.pl
+++ xorg/lib/Config/Model/models/Xorg/Device.pl
@@ -1,5 +1,6 @@
 [
           {
+            'class_description' => 'Driver-independent entries and Options',
             'name' => 'Xorg::Device',
             'element' => [
                            'Driver',
@@ -11,7 +12,8 @@
                              'choice' => [
                                            'radeon',
                                            'nvidia',
-                                           'vesa'
+                                           'vesa',
+                                           'fglrx'
                                          ]
                            },
                            'BusID',
@@ -28,7 +30,9 @@
                                                       }
                                                     ]
                                        },
-                             'type' => 'leaf'
+                             'type' => 'leaf',
+                             'description' => "This specifies the bus location of the graphics card. For PCI/AGP cards, the bus-id string has the form
+PCI:bus:device:function (e.g., \x{201c}PCI:1:0:0\x{201d} might be appropriate for an AGP card). This field is usually optional in single-head configurations when using the primary graphics card. In multi-head configurations, or when using a secondary graphics card in a single-head configuration, this entry is mandatory. Its main purpose is to make an unambiguous connection between the device section and the hardware it is representing. This information can usually be found by running the Xorg server with the -scanpci command line option."
                            },
                            'Screen',
                            {
@@ -44,6 +48,25 @@
                                                       }
                                                     ]
                                        },
+                             'type' => 'leaf',
+                             'description' => 'his option is mandatory for cards where a single PCI entity can drive more than one display (i.e., multiple CRTCs sharing a single graphics accelerator and video memory). One Device section is required for each head, and this parameter determines which head each of the Device sections applies to. The legal values of number range from 0 to one less than the total number of heads per entity. Most drivers require that the primary screen (0) be present.'
+                           },
+                           'Chipset',
+                           {
+                             'value_type' => 'uniline',
+                             'experience' => 'advanced',
+                             'type' => 'leaf'
+                           },
+                           'Ramdac',
+                           {
+                             'value_type' => 'uniline',
+                             'experience' => 'advanced',
+                             'type' => 'leaf'
+                           },
+                           'DacSpeed',
+                           {
+                             'value_type' => 'uniline',
+                             'experience' => 'advanced',
                              'type' => 'leaf'
                            },
                            'Option',
@@ -64,26 +87,13 @@
                                           '$f1 eq \'nvidia\'',
                                           {
                                             'config_class_name' => 'Xorg::Device::Nvidia'
+                                          },
+                                          '$f1 eq \'fglrx\'',
+                                          {
+                                            'config_class_name' => 'Xorg::Device::Fglrx'
                                           }
-                                        ]
-                           },
-                           'Chipset',
-                           {
-                             'value_type' => 'uniline',
-                             'experience' => 'advanced',
-                             'type' => 'leaf'
-                           },
-                           'Ramdac',
-                           {
-                             'value_type' => 'uniline',
-                             'experience' => 'advanced',
-                             'type' => 'leaf'
-                           },
-                           'DacSpeed',
-                           {
-                             'value_type' => 'uniline',
-                             'experience' => 'advanced',
-                             'type' => 'leaf'
+                                        ],
+                             'description' => 'Option flags may be specified in the Device sections. These include driver-specific options and driver-independent options.'
                            }
                          ]
           }
