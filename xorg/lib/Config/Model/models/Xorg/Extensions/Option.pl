71386331ade9c75478f5aea0847185ed543f8f0c
Dominique Dumont <domi.dumont@free.fr>
2009-03-11 15:46:33 +0000

    added extension option model
    --HG--
    extra : convert_revision : svn%3Aec020cd8-820c-41dc-a724-4838206f7038/trunk%40883

---
+++ xorg/lib/Config/Model/models/Xorg/Extensions/Option.pl
@@ -0,0 +1,17 @@
+[
+          {
+            'name' => 'Xorg::Extensions::Option',
+            'element' => [
+                           'Composite',
+                           {
+                             'value_type' => 'enum',
+                             'type' => 'leaf',
+                             'choice' => [
+                                           'Disable',
+                                           'Enable'
+                                         ]
+                           }
+                         ]
+          }
+        ]
+;
