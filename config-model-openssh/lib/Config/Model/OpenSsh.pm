d791abf68bf88d531d374f47f8b6717cb952c61a
Dominique Dumont <domi.dumont@free.fr>
2009-04-19 11:52:53 +0000

    v 1.205
    --HG--
    extra : convert_revision : svn%3Aec020cd8-820c-41dc-a724-4838206f7038/trunk%40938

--- config-model-openssh/lib/Config/Model/OpenSsh.pm
+++ config-model-openssh/lib/Config/Model/OpenSsh.pm
@@ -34,7 +34,7 @@ use File::Path ;
 use Parse::RecDescent ;
 use vars qw($VERSION $grammar $parser)  ;
 
-$VERSION = '1.204' ;
+$VERSION = '1.205' ;
 
 
 my $logger = Log::Log4perl::get_logger(__PACKAGE__);
