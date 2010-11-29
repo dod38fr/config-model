bbdbcfad55058079969ef7637f8da70ac2bfa79f
Dominique Dumont <domi@komarr.gre.hp.com>
2010-11-29 13:47:42 +0100

    improved model plugin doc

--- config-model-core/lib/Config/Model.pm
+++ config-model-core/lib/Config/Model.pm
@@ -1800,9 +1800,47 @@ sub _do_model_file {
 
 =head1 Model plugin
 
-TBD: For a proper model plugin, scan directory <model_name>.d and
-load in merge mode all pieces of model found there merge mode: model
-data is added to main model before running create_config_class
+Config::Model can also use model plugins. Each model can be augmented by model snippets
+stored into directory C<< <model_name>.d >>. All files found there will be merged to existing model.
+
+For instance, this model:
+
+ {
+    name => "Master",
+    element => [
+	fs_vfstype => {
+            type => 'leaf',
+            value_type => 'enum',
+            choice => [ qw/ext2 ext3/ ],
+        },
+        fs_mntopts => {
+            type => 'warped_node',
+            follow => { 'f1' => '- fs_vfstype' },
+            rules => [
+                '$f1 eq \'ext2\'', { 'config_class_name' => 'Fstab::Ext2FsOpt' },
+                '$f1 eq \'ext3\'', { 'config_class_name' => 'Fstab::Ext3FsOpt' }, 
+            ],
+        }
+    ]
+ }
+
+can be augmented with:
+
+ {
+    name => "Fstab::Fsline",
+    element => [
+ 	fs_vfstype => { choice => [ qw/ext4/ ], },
+        fs_mntopts => {
+            rules => [
+                q!$f1 eq 'ext4'!, { 'config_class_name' => 'Fstab::Ext4FsOpt' }, 
+            ],
+        },
+    ]
+ } ;
+ 
+Then, the merged model will feature C<fs_vfstype> with choice C<ext2 ext4 ext4>. 
+Likewise, C<fs_mntopts> will feature rules for the 3 filesystems. 
+
 
 =head2 augment_config_class (name => '...', ... )
 
