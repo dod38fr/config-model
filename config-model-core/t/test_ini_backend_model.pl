05896df5f5fce245caddfeb3bb2bc8f878f53668
Dominique Dumont <domi@komarr.gre.hp.com>
2010-12-19 14:38:27 +0100

    update accept statements in tests models

--- config-model-core/t/test_ini_backend_model.pl
+++ config-model-core/t/test_ini_backend_model.pl
@@ -1,103 +1,102 @@
 # test model used by t/*.t
 
 [
- {
-  read_config  => [
-                    {   backend => 'IniFile', 
-                        config_dir => '/etc/',
-                        file => 'test.ini',
-                        auto_create => 1,
-                    },
-                  ],
+    {
+        read_config => [
+            {
+                backend     => 'IniFile',
+                config_dir  => '/etc/',
+                file        => 'test.ini',
+                auto_create => 1,
+            },
+        ],
 
-  name => 'IniTest',
+        name => 'IniTest',
 
-  element =>      [
-                    [qw/foo bar/] => { 
-                                      type => 'list',
-                                      cargo => {
-                                                type => 'leaf',
-                                                value_type => 'uniline',
-                                                }
-                                     },
+        element => [
+            [qw/foo bar/] => {
+                type  => 'list',
+                cargo => {
+                    type       => 'leaf',
+                    value_type => 'uniline',
+                }
+            },
 
-                    [qw/class1 class2/] => { 
-                                            type => 'node',
-                                            config_class_name => 'IniTest::Class'
-                                           }
-                  ]
-  },
- {
-  read_config  => [
-                    {   backend => 'IniFile', 
-                        config_dir => '/etc/',
-                        file => 'test.ini',
-                        auto_create => 1,
-                        comment_delimiter => ';' ,
-                    },
-                  ],
+            [qw/class1 class2/] => {
+                type              => 'node',
+                config_class_name => 'IniTest::Class'
+            }
+        ]
+    },
+    {
+        read_config => [
+            {
+                backend           => 'IniFile',
+                config_dir        => '/etc/',
+                file              => 'test.ini',
+                auto_create       => 1,
+                comment_delimiter => ';',
+            },
+        ],
 
-  name => 'IniTest2',
+        name => 'IniTest2',
 
-  element =>      [
-                    [qw/foo bar/] => { 
-                                      type => 'list',
-                                      cargo => {
-                                                type => 'leaf',
-                                                value_type => 'uniline',
-                                                }
-                                     },
+        element => [
+            [qw/foo bar/] => {
+                type  => 'list',
+                cargo => {
+                    type       => 'leaf',
+                    value_type => 'uniline',
+                }
+            },
 
-                    [qw/class1 class2/] => { 
-                                            type => 'node',
-                                            config_class_name => 'IniTest::Class'
-                                           }
-                  ]
-  },{
-  name => 'IniTest::Class',
-  element => [
-              [qw/lista listb/] => {
-                                    type => 'list',
-                                    cargo => { 
-                                              type => 'leaf',
-                                              value_type => 'uniline',
-                                             } ,
-                                   },
-             ]
-  }, 
-   {
-     name => 'AutoIni',
-    read_config  => [
-                    {   backend => 'IniFile', 
-                        config_dir => '/etc/',
-                        file => 'test.ini',
-                        auto_create => 1,
-                    },
-                  ],
-     accept => [
-                      {
-                        'name_match' => 'class.*',
-                        'type' => 'node',
-                        'config_class_name' => 'AutoIniClass'
-                      },
-                      {
-                        'type' => 'list',
-                        cargo => { qw/type leaf value_type uniline/ },
-                      }
-                    ],
-      },
-      {
-        name => 'AutoIniClass',
+            [qw/class1 class2/] => {
+                type              => 'node',
+                config_class_name => 'IniTest::Class'
+            }
+        ]
+    },
+    {
+        name    => 'IniTest::Class',
+        element => [
+            [qw/lista listb/] => {
+                type  => 'list',
+                cargo => {
+                    type       => 'leaf',
+                    value_type => 'uniline',
+                },
+            },
+        ]
+    },
+    {
+        name        => 'AutoIni',
+        read_config => [
+            {
+                backend     => 'IniFile',
+                config_dir  => '/etc/',
+                file        => 'test.ini',
+                auto_create => 1,
+            },
+        ],
         accept => [
-                      {
-                       'type' => 'list',
-                        cargo => { qw/type leaf value_type uniline/ },
-                     }
-                    ],
-      }
-
+            'class.*' => {
+                'type'              => 'node',
+                'config_class_name' => 'AutoIniClass'
+            },
+            '.*' => {
+                'type' => 'list',
+                cargo  => {qw/type leaf value_type uniline/},
+            }
+        ],
+    },
+    {
+        name   => 'AutoIniClass',
+        accept => [
+            '.*' => {
+                'type' => 'list',
+                cargo  => {qw/type leaf value_type uniline/},
+            }
+        ],
+    }
 
 ];
-
-
-
