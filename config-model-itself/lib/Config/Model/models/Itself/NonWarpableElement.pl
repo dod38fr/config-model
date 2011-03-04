62dbe06f88f22c334fb84705cf96057172fcdffe
Dominique Dumont <domi@komarr.gre.hp.com>
2011-03-04 16:58:21 +0100

    tidied

--- config-model-itself/lib/Config/Model/models/Itself/NonWarpableElement.pl
+++ config-model-itself/lib/Config/Model/models/Itself/NonWarpableElement.pl
@@ -17,187 +17,189 @@
 #    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
 
 [
- [
-  name => 'Itself::NonWarpableElement',
+    [
+        name => 'Itself::NonWarpableElement',
 
-  # warp often depend on this one, so list it first
-  'element'
-  => [
-      'value_type' 
-      => { type => 'leaf',
-	   level => 'hidden',
-	   value_type => 'enum',
-	   'warp'
-	   => { follow => { 't' => '- type' },
-		'rules'
-		=> [ '$t eq "leaf"' 
-		     => {
-			 choice => [qw/boolean enum integer reference
-				       number uniline string/],
-			 level => 'normal',
-			 mandatory => 1,
-			}
-		   ]
-	      }
-	 },
+        # warp often depend on this one, so list it first
+        'element' => [
+            'value_type' => {
+                type       => 'leaf',
+                level      => 'hidden',
+                value_type => 'enum',
+                'warp'     => {
+                    follow  => { 't' => '- type' },
+                    'rules' => [
+                        '$t eq "leaf"' => {
+                            choice => [
+                                qw/boolean enum integer reference
+                                  number uniline string/
+                            ],
+                            level     => 'normal',
+                            mandatory => 1,
+                        }
+                    ]
+                }
+            },
 
-      'class' 
-      => { type => 'leaf',
-	   level => 'hidden',
-	   value_type => 'uniline',
-	   summary => "Override Config::Model::Value:",
-	   description => "Perl class name of a child of Config::Model::Value",
-	   experience => 'master',
-	   'warp'
-	   => { follow => { 't' => '- type' },
-		'rules'
-		=> [ '$t eq "leaf"' 
-		     => {
-			 level => 'normal',
-			}
-		   ]
-	      }
-	 },
+            'class' => {
+                type       => 'leaf',
+                level      => 'hidden',
+                value_type => 'uniline',
+                summary    => "Override Config::Model::Value:",
+                description =>
+                  "Perl class name of a child of Config::Model::Value",
+                experience => 'master',
+                'warp'     => {
+                    follow  => { 't'            => '- type' },
+                    'rules' => [ '$t eq "leaf"' => { level => 'normal', } ]
+                }
+            },
 
+            # node element (may be within a hash or list)
 
-      # node element (may be within a hash or list)
+            # warped_node: warp parameter for warped_node. They must be
+            # warped out when type is not a warped_node
+            'follow' => {
+                type       => 'hash',
+                index_type => 'string',
+                level      => 'hidden',
+                'warp'     => {
+                    follow  => '- type',
+                    'rules' => { 'warped_node' => { level => 'normal', }, }
+                },
+                cargo => {
+                    type       => 'leaf',
+                    value_type => 'uniline'
+                },
+                description =>
+                  "Specifies the path to the value elements that drive the "
+                  . "change of this node. Each key of the has is a variable name used "
+                  . "in the 'rules' parameter. The value of the hash is a path in the "
+                  . "configuration tree",
+            },
 
-      # warped_node: warp parameter for warped_node. They must be
-      # warped out when type is not a warped_node
-      'follow' => 
-      => {
-	  type => 'hash',
-	  index_type => 'string',
-	  level      => 'hidden' ,
-	  'warp'
-	  => { follow => '- type',
-	       'rules' => { 'warped_node' => {level => 'normal',},}
-	     },
-	  cargo => { type => 'leaf',
-		     value_type => 'uniline' 
-		   },
-	  description => "Specifies the path to the value elements that drive the "
-			 ."change of this node. Each key of the has is a variable name used "
-			 ."in the 'rules' parameter. The value of the hash is a path in the "
-			 ."configuration tree",
-	 },
+            'morph' => {
+                type       => 'leaf',
+                level      => 'hidden',
+                value_type => 'boolean',
+                'warp'     => {
+                    follow  => '- type',
+                    'rules' => {
+                        'warped_node' => {
+                            level            => 'normal',
+                            upstream_default => 0,
+                        },
+                    }
+                },
+                description =>
+                  "When set, a recurse copy of the value from the old object "
+                  . "to the new object will be attemped. When a copy is not possible, "
+                  . "undef values will be assigned.",
+            },
 
-      'morph' => 
-      => {
-	  type => 'leaf',
-	  level      => 'hidden' ,
-	  value_type => 'boolean', 
-	  'warp'
-	  => { follow => '- type',
-	       'rules'
-	       => { 'warped_node' 
-		    => {
-			level => 'normal',
-			upstream_default => 0 ,
-		       },
-		  }
-	     },
-	  description => "When set, a recurse copy of the value from the old object "
-	       ."to the new object will be attemped. When a copy is not possible, "
-		    ."undef values will be assigned.",
-	 },
+            # end warp elements for warped_node
 
-      # end warp elements for warped_node
+            # leaf element
 
-      # leaf element
+            'refer_to' => {
+                type       => 'leaf',
+                level      => 'hidden',
+                value_type => 'uniline',
+                experience => 'advanced',
+                warp       => {
+                    follow => {
+                        t  => '- type',
+                        vt => '- value_type',
+                    },
+                    'rules' => [
+                        '$t  eq "check_list" or $vt eq "reference"' =>
+                          { level => 'important', },
+                    ]
+                },
+                description =>
+                  "points to an array or hash element in the configuration "
+                  . "tree using the path syntax. The available choice of this "
+                  . "reference value (or check list)is made from the available "
+                  . "keys of the pointed hash element or the values of the pointed array element.",
+            },
 
-      'refer_to' 
-      => { type => 'leaf',
-	   level      => 'hidden' ,
-	   value_type => 'uniline',
-	   experience => 'advanced',
-	   warp => { follow => { t  => '- type',
-				 vt => '- value_type',
-			       },
-		     'rules'
-		     => [ '   $t  eq "check_list"
-                           or $vt eq "reference"  '
-			  => {
-			      level => 'important',
-			     },
-			]
-		   },
-	   description => "points to an array or hash element in the configuration "
-			 ."tree using the path syntax. The available choice of this "
-			 ."reference value (or check list)is made from the available "
-			 ."keys of the pointed hash element or the values of the pointed array element.",
-	 },
+            'computed_refer_to' => {
+                type   => 'warped_node',
+                follow => {
+                    t  => '- type',
+                    vt => '- value_type',
+                },
+                level      => 'hidden',
+                experience => 'master',
+                'rules'    => [
+                    '$t  eq "check_list" or $vt eq "reference"' => {
+                        level             => 'normal',
+                        config_class_name => 'Itself::ComputedValue',
+                    },
+                ],
+                description =>
+                  "points to an array or hash element in the configuration "
+                  . "tree using a path computed with value from several other "
+                  . "elements in the configuration tree. The available choice "
+                  . "of this reference value (or check list) is made from the "
+                  . "available keys of the pointed hash element or the values "
+                  . "of the pointed array element.",
+            },
 
-      'computed_refer_to' 
-      => { type => 'warped_node',
-	   follow => { t  => '- type',
-		       vt => '- value_type',
-		     },
-	   level => 'hidden',
-	   experience => 'master',
-	   'rules'
-	   => [ '   $t  eq "check_list" 
-                 or $vt eq "reference"  '
-		=> {
-		    level => 'normal',
-		    config_class_name => 'Itself::ComputedValue',
-		   },
-	      ],
-	   description => "points to an array or hash element in the configuration tree using a path computed with value from several other elements in the configuration tree. The available choice of this reference value (or check list) is made from the available keys of the pointed hash element or the values of the pointed array element.",
-	 },
+            'replace_follow' => {
+                type       => 'leaf',
+                level      => 'hidden',
+                value_type => 'uniline',
+                experience => 'advanced',
+                warp       => {
+                    follow  => { t               => '- type' },
+                    'rules' => [ '$t  eq "leaf"' => { level => 'important', }, ]
+                },
+                description =>
+                  "Path specifying a hash of value element in the configuration "
+                  . "tree. The hash if used in a way similar to the replace "
+                  . "parameter. In this case, the replacement is not coded "
+                  . "in the model but specified by the configuration.",
+            },
 
-      'replace_follow' 
-      => { type => 'leaf',
-	   level      => 'hidden' ,
-	   value_type => 'uniline',
-	   experience => 'advanced',
-	   warp => { follow => { t  => '- type'},
-		     'rules'
-		     => [ '$t  eq "leaf"'
-			  => {
-			      level => 'important',
-			     },
-			]
-		   },
-	   description => "Path specifying a hash of value element in the configuration tree. The hash if used in a way similar to the replace parameter. In this case, the replacement is not coded in the model but specified by the configuration.",
-	 },
+            'compute' => {
+                type       => 'warped_node',
+                level      => 'hidden',
+                experience => 'advanced',
 
-      'compute' 
-      => { type => 'warped_node',
-	   level => 'hidden',
-	   experience => 'advanced',
+                follow  => { t => '- type', },
+                'rules' => [
+                    '$t  eq "leaf"' => {
+                        level             => 'normal',
+                        config_class_name => 'Itself::ComputedValue',
+                    },
+                ],
+                description =>
+                  "compute the default value according to a formula and value "
+                  . "from other elements in the configuration tree.",
+            },
 
-	   follow => { t  => '- type',},
-	   'rules' => [ '$t  eq "leaf"'
-			=> {
-			    level => 'normal',
-			    config_class_name => 'Itself::ComputedValue',
-			   },
-		      ],
-	   description => "compute the default value according to a formula and value from other elements in the configuration tree.",
-	 },
+            'migrate_from' => {
+                type       => 'warped_node',
+                level      => 'hidden',
+                experience => 'advanced',
 
-      'migrate_from' 
-      => { type => 'warped_node',
-	   level => 'hidden',
-	   experience => 'advanced',
+                follow  => { t => '- type', },
+                'rules' => [
+                    '$t  eq "leaf"' => {
+                        level             => 'normal',
+                        config_class_name => 'Itself::MigratedValue',
+                    },
+                ],
+                description =>
+"                  Specify an upgrade path from an old value and compute "
+                  . "the value to store in the new element.",
+            },
 
-	   follow => { t  => '- type',},
-	   'rules' => [ '$t  eq "leaf"'
-			=> {
-			    level => 'normal',
-			    config_class_name => 'Itself::MigratedValue',
-			   },
-		      ],
-	   description => "Specify an upgrade path from an old value and compute the value to store in the new element.",
-	 },
+            # hash element
 
+            # list element
 
-      # hash element
-
-      # list element
-
-
-     ],
- ],
+        ],
+    ],
 ];
