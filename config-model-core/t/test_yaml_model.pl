a6193e5ee04df5ad62d2b034294b7abb549ac53c
Dominique Dumont <domi@komarr.gre.hp.com>
2011-04-04 13:00:00 +0200

    refromatted

--- config-model-core/t/test_yaml_model.pl
+++ config-model-core/t/test_yaml_model.pl
@@ -1,35 +1,37 @@
 # test model used by t/*.t
 
 [
- {
-  name => 'Host',
+    {
+        name => 'Host',
 
-  element => [
-	      [qw/ipaddr canonical alias/] 
-	      => { type => 'leaf',
-		   value_type => 'uniline',
-		 } 
-	     ]
- },
- {
-  name => 'Hosts',
+        element => [
+            [qw/ipaddr canonical alias/] => {
+                type       => 'leaf',
+                value_type => 'uniline',
+            },
+            dummy => {qw/type leaf value_type uniline upstream_default toto/},
+        ]
+    },
+    {
+        name => 'Hosts',
 
-  read_config  => [ { backend => 'yaml', 
-		      config_dir => '/yaml/',
-		      file => 'hosts.yml',
-		      auto_create => 1,
-		    },
-		  ],
+        read_config => [
+            {
+                backend     => 'yaml',
+                config_dir  => '/yaml/',
+                file        => 'hosts.yml',
+                auto_create => 1,
+            },
+        ],
 
-  element => [
-	      record => { type => 'list',
-			  cargo => { type => 'node',
-				     config_class_name => 'Host',
-				   } ,
-			},
-	     ]
- }
+        element => [
+            record => {
+                type  => 'list',
+                cargo => {
+                    type              => 'node',
+                    config_class_name => 'Host',
+                },
+            },
+        ]
+    }
 ];
-
-
-
