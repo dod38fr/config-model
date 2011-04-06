c509309dda29a429f1ef74dc0f50e984d135fedf
Dominique Dumont <domi@komarr.gre.hp.com>
2011-04-06 16:23:15 +0200

    added doc

--- config-model-core/lib/Config/Model/models/Debian/Dpkg/Source.pl
+++ config-model-core/lib/Config/Model/models/Debian/Dpkg/Source.pl
@@ -1,5 +1,7 @@
 [
   {
+    'class_description' => 'Model of files found under debian/source directory. 
+See L<dpkg-source> for details.',
     'read_config' => [
       {
         'auto_create' => '1',
@@ -19,8 +21,21 @@
       'format',
       {
         'value_type' => 'enum',
+        'summary' => 'source package format',
+        'help' => {
+          '2.0' => 'Wig&pen was the first specification of a new-generation source package format. This format is not recommended for wide-spread usage, the format "3.0 (quilt)" replaces it.',
+          '3.0 (quilt)' => 'A source package in this format contains at least an original tarball (.orig.tar.ext where ext can be gz, bz2, lzma and xz) and a debian tarball (.debian.tar.ext). It can also contain additional original tarballs (.orig-component.tar.ext).',
+          '3.0 (native)' => 'extension of the native package format as defined in the 1.0 format.',
+          '3.0 (custom)' => 'This format is particular. It doesn\'t represent a real source package format but can be used to create source packages with arbitrary files.
+',
+          '3.0 (bzr)' => 'This format is experimental. It generates a single tarball containing the bzr repository.
+',
+          '1.0' => 'A source package in this format consists either of a .orig.tar.gz associated to a .diff.gz or a single .tar.gz (in that case the package is said to be native).',
+          '3.0 (git)' => "This format is experimental. A source package in this format consists of a single bundle of a git repository .git to hold the source of a package. There may also be a .git\x{2010} shallow file listing revisions for a shallow git clone."
+        },
         'mandatory' => '1',
         'type' => 'leaf',
+        'description' => 'Specifies the format of the source package. A missing format implies a \'1.0\' source format.',
         'choice' => [
           '1.0',
           '2.0',
