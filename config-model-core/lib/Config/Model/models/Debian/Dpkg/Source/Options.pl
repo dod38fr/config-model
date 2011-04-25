671701a3731d364954b2312d32d379ee547c418f
Dominique Dumont <domi.dumont@free.fr>
2011-04-25 17:26:47 +0200

    fix spelling issues

--- config-model-core/lib/Config/Model/models/Debian/Dpkg/Source/Options.pl
+++ config-model-core/lib/Config/Model/models/Debian/Dpkg/Source/Options.pl
@@ -29,7 +29,7 @@
         'value_type' => 'uniline',
         'summary' => 'perl regexp to filter out files for the diff',
         'type' => 'leaf',
-        'description' => "perl regular expression to match files you want filtered out of the list of files for the diff.This is very helpful in cutting out extraneous files that get included in the diff, e.g. if you maintain your source in a revi\x{2010} sion control system and want to use a checkout to build a source package without including the additional files and directories that it will usually contain (e.g. CVS/, .cvsignore, .svn/). The default regexp is already very exhaustive, but if you need to replace it, please note that by default it can match any part of a path, so if you want to match the begin of a filename or only full filenames, you will need to provide the necessary anchors (e.g. '(^|/)', '(\$|/)') yourself."
+        'description' => "perl regular expression to match files you want filtered out of the list of files for the diff.This is very helpful in cutting out extraneous files that get included in the diff, e.g. if you maintain your source in a revision control system and want to use a checkout to build a source package without including the additional files and directories that it will usually contain (e.g. CVS, .cvsignore, .svn/). The default regexp is already very exhaustive, but if you need to replace it, please note that by default it can match any part of a path, so if you want to match the begin of a filename or only full filenames, you will need to provide the necessary anchors (e.g. '(^|/)', '(\$|/)') yourself."
       },
       'extend-diff-ignore',
       {
