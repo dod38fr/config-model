889118feed96c643bbe01764b597e0824b68e712
Dominique Dumont <domi@komarr.gre.hp.com>
2011-06-09 15:37:13 +0200

    reworked to be more easy to understand (comments, var name...)

--- config-model-core/examples/lcdproc/lcdconf2model.pl
+++ config-model-core/examples/lcdproc/lcdconf2model.pl
@@ -3,25 +3,63 @@
 use strict;
 use warnings;
 
+# This script uses all the information available in LCDd.conf to create a model
+# for LCDd configuration file
+
+# How does this work ?
+# The convention used in LCDd.conf template file are written in a way which
+# makes it relatively easy to parse to get all required information to build a model.
+# All drivers are listed, most parameters have the default values and legal values
+# written in comments in a uniform way. Hence this file (and comments) can be parsed
+# to retrieve information required for the model.
+
+# this script performs 3 tasks:
+# 1/ parse LCDd.conf template
+# 2/ mine the information there and translate these in a format suitable to create
+#    a model. Comments are used to provide default and legal values and also to provide
+#    user documentation
+# 3/ Write the resulting LCDd model
+
 use Config::Model;
-use Config::Model::Itself;
+use Config::Model::Itself;    # to create the model
 use Config::Model::Backend::IniFile;
+
+use 5.10.0;
 use IO::File;
 use IO::String;
+
+# initialise logs for Config;:Model
 use Log::Log4perl qw(:easy);
-use 5.10.0;
-
 my $log4perl_user_conf_file = $ENV{HOME} . '/.log4config-model';
-
 Log::Log4perl::init($log4perl_user_conf_file);
 
+# one model to rule them all
 my $model = Config::Model->new();
 
-# load LCDd.conf in a dummy model to benefit from INI parser.
+###########################
+#
+# Step 1: parse LCDd.conf (INI file)
+
+# Problem: comments must also be retrieved and associated with INI
+# class and parameters
+
+# Fortunately, Config::Model::Backend::IniFile can already to this.
+# But Config::Model::Backend::IniFile must store its values in a
+# configuration tree.
+
+# So let's create a model suitable for LCDd.conf that accepts any
+# INI class and any INI value
+
+# The class used to store any parameter
 $model->create_config_class(
     name   => 'Dummy::Class',
     accept => [ '.*' => {qw/type leaf value_type uniline/}, ],
 );
+
+# Store any INI class, and use Dummy::Class to hold parameters Note that
+# a INI backend could be created here. But, some useful parameters are
+# commented out in LCD.conf so some some processing is required to be
+# able to create a model with these commented parameters. See below
 
 $model->create_config_class(
     name   => 'Dummy',
@@ -37,19 +75,28 @@ my $dummy = $dummy_inst->config_root;
 
 $dummy->init;
 
-# read file myself as some pre-process is required
-my $ini_backend = Config::Model::Backend::IniFile->new( node => $dummy );
-
+# read LCDd.conf
 my $lcd_file = IO::File->new('examples/lcdproc/LCDd.conf');
 my @lines    = $lcd_file->getlines;
 
-# restore commented lines
+# un-comment commented parameters
 foreach (@lines) { s/^#(\w+=)/$1/ }
+
+# store the munged LCDd.conf in a IO::Handle
 my $ioh = IO::String->new( join( '', @lines ) );
 
+# Create the INI backend
+my $ini_backend = Config::Model::Backend::IniFile->new( node => $dummy );
+
+# read the munged LCDd.conf content
 $ini_backend->read( io_handle => $ioh );
 
-# load Config::Model model
+##############################################
+#
+# Step 2: Mine the LCDd.conf information and create a model
+#
+
+# Create a meta_model that will contain LCDd model
 my $meta_inst = $model->instance(
     root_class_name => 'Itself::Model',
     instance_name   => 'meta_model',
@@ -57,18 +104,23 @@ my $meta_inst = $model->instance(
 
 my $meta_root = $meta_inst->config_root;
 
-# read dummy file to create model_object
-my $extra_description = "Model information extracted from template /etc/LCDd.conf"
-    ."\n\n=head1 BUGS\n\nThis model does not support to load several drivers. Loading "
-    ."several drivers is probably a marginal case. Please complain to the author if this "
-    ."assumption is false" ;
-    
+# Create some documentation for end user
+my $extra_description =
+    "Model information extracted from template /etc/LCDd.conf"
+  . "\n\n=head1 BUGS\n\nThis model does not support to load several drivers. Loading "
+  . "several drivers is probably a marginal case. Please complain to the author if this "
+  . "assumption is false";
 
+# Create LCDd configuration class and store the first comment from LCDd.conf as
+# class description
 $meta_root->grab("class:LCDd class_description")->store( $dummy->annotation );
-$meta_root->load( qq!class:LCDd class_description.="\n\n$extra_description"! );
 
-$meta_root->load(
-qq!class:LCDd 
+# append my own text
+$meta_root->load(qq!class:LCDd class_description.="\n\n$extra_description"!);
+
+# add legal stuff and backend (So LCDd model will be able to read INI files)
+$meta_root->load( qq!
+    class:LCDd 
     copyright:0="2011, Dominique Dumont" 
     copyright:1="1999-2011, William Ferrell and others" 
     license="GPL-2"
@@ -81,108 +133,167 @@ qq!class:LCDd
 !
 );
 
+# This array contains all INI classes found in LCDd.conf
 my @ini_classes = $dummy->get_element_name;
-print "LCDd ini_classes: @ini_classes\n";
 
-my %def;
-%def = (
-    "LCDd::server" => {
-        Driver => sub {
-            my ( $class, $elt, $info, $ini_v ) = @_;
-            my $load = qq!class:"$class" element:$elt type=leaf value_type=enum !;
-            my @drivers = split /\W+/, $info;
-            while ( @drivers and ( shift @drivers ) !~ /supported/ ) { }
-            $load .= 'choice=' . join( ',', @drivers ) . ' ';
+# Now before actually mining LCDd.conf information, we must prepare
+# subs to handle them. This is done using a dispatch table.
+my %dispatch;
 
-            #print $load; exit;
-            return $load;
-        },
-        DriverPath => sub {
-            my ( $class, $elt, $info, $ini_v ) = @_;
-            return $def{_default_}->(@_) . q! match="/$" !;
-        },
-        WaitTime    => $def{LCDd}{Port},
-        ReportLevel => $def{LCDd}{Port},
-        Port        => sub {
-            my ( $class, $elt, $info, $ini_v ) = @_;
-            return $def{_default_}->( @_, 'integer' );
-        },
-        GoodBye => $def{LCDd}{Hello},
-        Hello   => sub {
-            my ( $class, $elt, $info, $ini_v ) = @_;
-            return qq(class:"$class" element:$elt type=list 
-               cargo type=leaf value_type=uniline - 
-               default_with_init:0="    $elt" default_with_init:1="    LCDproc!" );
-        },
-    },
-    _default_ => sub {
-        my ( $class, $elt, $ini_note, $ini_v, $value_type ) = @_;
-        my $load = qq!class:"$class" element:$elt type=leaf !;
-        $value_type ||= 'uniline';
+# first create the default case which will be used for most parameters
+# This subs is passed: the INI class name, the INI parameter name
+# the comment attached to the parameter, the INI value, and an optional
+# value type  
+$dispatch{_default_} = sub {
+    my ( $ini_class, $ini_param, $ini_note, $ini_v, $value_type ) = @_;
 
-        my $info = $ini_note =~ /\[(.*)\]/ ? $1 : '';
-        $info =~ s/\s+//g;
-        $load .=
-            $info =~ /legal:(\d+)-(\d+)/ ? " value_type=integer min=$1 max=$2"
-          : $info =~ /legal:([\w\,]+)/   ? " value_type=enum choice=$1"
-          :                                " value_type=$value_type";
-        if ( $info =~ /default:(\w+)/ ) {
-            $load .= qq! upstream_default="$1"! if length($1);
-        }
-        else {
-            $ini_v =~ s/^"//g;
-            $ini_v =~ s/"$//g;
-            $load .= qq! default="$ini_v"! if length($ini_v);
-        }
-        return $load;
+    # prepare a string to create the ini_class model
+    my $load = qq!class:"$ini_class" element:$ini_param type=leaf !;
+    $value_type ||= 'uniline';
+
+    # get semantic information from comment (written between square barckets)
+    my $info = $ini_note =~ /\[(.*)\]/ ? $1 : ''; 
+    $info =~ s/\s+//g;
+    # use this semantic information to better specify the parameter
+    $load .=
+        $info =~ /legal:(\d+)-(\d+)/ ? " value_type=integer min=$1 max=$2"
+      : $info =~ /legal:([\w\,]+)/   ? " value_type=enum choice=$1"
+      :                                " value_type=$value_type";
+
+    if ( $info =~ /default:(\w+)/ ) {
+        # specify upstream default value if it was found in the comment
+        $load .= qq! upstream_default="$1"! if length($1);
     }
-);
+    else {
+        # or use the value found in INI file as default
+        $ini_v =~ s/^"//g;
+        $ini_v =~ s/"$//g;
+        $load .= qq! default="$ini_v"! if length($ini_v);
+    }
+    
+    # return a string containing model specifications
+    return $load;
+};
 
-# handle server and menu elements
-foreach my $item (@ini_classes) {
-    my $ini_obj = $dummy->grab($item);
-    my $class = "LCDd::$item";
-    
+# Now let's take care of the special cases. This one deals with "Driver"
+# parameter found in INI [server] class
+$dispatch{"LCDd::server"}{Driver} = sub {
+    my ( $class, $elt, $info, $ini_v ) = @_;
+    my $load = qq!class:"$class" element:$elt type=leaf value_type=enum !;
+    my @drivers = split /\W+/, $info;
+    while ( @drivers and ( shift @drivers ) !~ /supported/ ) { }
+    $load .= 'choice=' . join( ',', @drivers ) . ' ';
+
+    #say $load; exit;
+    return $load;
+};
+
+# like default but ensure that DriverPath ends with '/'
+$dispatch{"LCDd::server"}{DriverPath} = sub {
+    my ( $class, $elt, $info, $ini_v ) = @_;
+    return $dispatch{_default_}->(@_) . q! match="/$" !;
+};
+
+# like default but ensure that parameter is integer
+$dispatch{"LCDd::server"}{WaitTime} = $dispatch{"LCDd::server"}{ReportLevel} =
+  $dispatch{"LCDd::server"}{Port}   = sub {
+    my ( $class, $elt, $info, $ini_v ) = @_;
+    return $dispatch{_default_}->( @_, 'integer' );
+  };
+
+# ensure that default values are "Hello LCDproc" (or "GoodBye LCDproc")
+$dispatch{"LCDd::server"}{GoodBye} = $dispatch{"LCDd::server"}{Hello} = sub {
+    my ( $class, $elt, $info, $ini_v ) = @_;
+    return qq(
+    class:"$class" 
+        element:$elt 
+        type=list 
+        cargo 
+            type=leaf 
+            value_type=uniline - 
+        default_with_init:0="    $elt" 
+        default_with_init:1="    LCDproc!" 
+    );
+};
+
+# Now really mine LCDd.conf information
+
+# loop over all INI classes
+foreach my $ini_class (@ini_classes) {
+    say "Handling INI class $ini_class";
+    my $ini_obj = $dummy->grab($ini_class);
+    my $config_class   = "LCDd::$ini_class";
+
     # create config class in case there's no parameter in INI file
-    $meta_root->load(qq!class:"LCDd::$item"!);
-    
-    foreach my $elt ( $ini_obj->get_element_name ) {
-        my $ini_v    = $ini_obj->grab_value($elt);
-        my $ini_note = $ini_obj->grab($elt)->annotation;
-        $ini_note =~ s/"/'/g;
+    $meta_root->load(qq!class:"LCDd::$ini_class"!);
+
+    # loop over all INI parameters
+    foreach my $ini_param ( $ini_obj->get_element_name ) {
+        # retrieve INI value
+        my $ini_v    = $ini_obj->grab_value($ini_param);
+
+        # retrieve INI comment attached to $ini_param
+        my $ini_comment = $ini_obj->grab($ini_param)->annotation;
+        # embedded quotes causes trouble, replace them with single quotes.
+        $ini_comment =~ s/"/'/g;
+
+        # remove unnecessary quotes
         $ini_v    =~ s/^"//g;
         $ini_v    =~ s/"$//g;
 
-        my $sub = $def{$class}{$elt} || $def{_default_};
-        my $load = $sub->( $class, $elt, $ini_note, $ini_v );
+        # retrieve the correct sub from the dispatch table
+        my $sub = $dispatch{$config_class}{$ini_param} || $dispatch{_default_};
+        
+        # runs the sub to get the model string
+        my $model_spec = $sub->( $config_class, $ini_param, $ini_comment, $ini_v );
 
-        $load .= qq! description="$ini_note"! if length($ini_note);
-        print "Class load:$load\n";
-        $meta_root->load($load);
+        $model_spec .= qq! description="$ini_comment"! if length($ini_comment);
+
+        # load class specification in model
+        $meta_root->load($model_spec);
     }
-    
-    if ($item ne 'server' and $item ne 'menu') {
-        my $load = qq!class:LCDd element:$item type=warped_node
-            config_class_name="LCDd::$item" level=hidden
-            follow:selected="- server Driver"
-            rules:"\$selected eq '$item'" level=normal! ;
-        print "Class load:$load\n";
-        $meta_root->load($load) ;
+
+    if ( $ini_class ne 'server' and $ini_class ne 'menu' ) {
+        # Arrange a driver class is shown only if the driver was selected
+        # in the [server] class
+        my $driver_class_spec = qq!
+            class:LCDd 
+                element:$ini_class 
+                type=warped_node 
+                config_class_name="LCDd::$ini_class"
+                level=hidden
+                follow:selected="- server Driver"
+                rules:"\$selected eq '$ini_class'" 
+                    level=normal
+            !;
+        $meta_root->load($driver_class_spec);
     }
 }
 
 say "Creating LCDs root elements (server and menu)";
 
-$meta_root->load(qq!
-  class:LCDd  
-    element:server  type=node config_class_name="LCDd::server" -
-    element:menu    type=node config_class_name="LCDd::menu" -
-!);
+# create the root LCDd class
+$meta_root->load(
+    qq!
+    class:LCDd  
+        element:server  
+            type=node 
+            config_class_name="LCDd::server" -
+        element:menu
+            type=node
+            config_class_name="LCDd::menu" -
+    !
+);
 
-#print $meta_root->dump_tree ;
+######################
+#
+# Step3: write the model 
+
 
 # Itself constructor returns an object to read or write the data
 # structure containing the model to be edited
 my $rw_obj = Config::Model::Itself->new( model_object => $meta_root );
+say "Writing all models in file (please wait)";
 $rw_obj->write_all( model_dir => 'lib/Config/Model/models/' );
+
 say "Done";
