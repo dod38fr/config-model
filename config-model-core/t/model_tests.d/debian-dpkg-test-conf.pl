ac0cc8d1456a3f5066687e0e4c10f01cb1634651
Dominique Dumont <domi@komarr.gre.hp.com>
2011-07-21 18:06:04 +0200

    updated for dep3 tests
    --HG--
    branch : dep3

--- config-model-core/t/model_tests.d/debian-dpkg-test-conf.pl
+++ config-model-core/t/model_tests.d/debian-dpkg-test-conf.pl
@@ -1,8 +1,8 @@
 use Data::Dumper ;
 use IO::File ;
 
-$conf_file_name = "control" ;
-$conf_dir='debian' ;
+$conf_file_name = "" ;
+$conf_dir='' ;
 $model_to_test = "Debian::Dpkg" ;
 
 eval { require AptPkg::Config ;} ;
@@ -16,27 +16,32 @@ $skip = ( $@ or not -r '/etc/debian_version') ? 1 : 0 ;
      dump_warnings => [ (qr/deprecated/) x 3 ],
      #errors => [ ],
     },
-    #{ #t1
-    # check => { 
-    #          },
-    # },
+    { #t1
+     check => { 
+                'patches:fix-spelling Synopsis', 'fix man page spelling',
+              },
+    },
 );
 
-my $cache_file = 't/model_tests.d/debian-dependency-cache.pl' ;
+my $cache_file = 't/model_tests.d/debian-dependency-cache.txt';
 
-unless (my $return = do $cache_file ) {
-        warn "couldn't parse $cache_file: $@" if $@;
-        warn "couldn't do $cache_file: $!"    unless defined $return;
-        warn "couldn't run $cache_file"       unless $return;
-    }
+my $ch = new IO::File "$cache_file";
+foreach ($ch->getlines) {
+    chomp;
+    my ($k,$v) = split m/ => / ;
+    $Config::Model::Debian::Dependency::cache{$k} = $v ;
+}
+$ch -> close ;
 
 END {
     return if $::DebianDependencyCacheWritten ;
-    my $str = Data::Dumper->Dump([\%Config::Model::Debian::Dependency::cache], ['*Config::Model::Debian::Dependency::cache']);
+    my %h = %Config::Model::Debian::Dependency::cache ;
+    my $str = join ("\n", map { "$_ => $h{$_}" ;} sort keys %h) ;
+
     my $fh = new IO::File "> $cache_file";
     print "writing back cache file\n";
-    if (defined $fh) {
-        # not a bit deal if cache cannot be written back
+    if ( defined $fh ) {
+        # not a big deal if cache cannot be written back
         $fh->print($str);
         $fh->close;
         $::DebianDependencyCacheWritten=1;
