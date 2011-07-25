4aa232b4bfa781e3a2a98932eccfe73e10325e0b
Dominique Dumont <domi@komarr.gre.hp.com>
2011-07-25 13:40:50 +0200

    patch write is working

--- config-model-core/lib/Config/Model/Backend/Debian/Dpkg/Patch.pm
+++ config-model-core/lib/Config/Model/Backend/Debian/Dpkg/Patch.pm
@@ -16,6 +16,8 @@ my $logger = get_logger("Backend::Debian::Dpkg::Patch");
 
 sub suffix { return ''; }
 
+sub skip_open { 1;}
+
 sub read {
     my $self = shift;
     my %args = @_;
@@ -66,6 +68,7 @@ sub read {
             unshift @elt, 'Synopsis' if $found eq 'Description';
             foreach (@elt) {
                 my $sub_v = shift @v;
+                next unless defined $sub_v ;
                 $logger->debug("storing $_  value: $sub_v");
                 $node->fetch_element($_)->store( value => $sub_v, check => $check );
             }
@@ -75,50 +78,6 @@ sub read {
     $node->fetch_element('diff')->store(join('',@$diff));
 
     return 1;
-}
-
-#
-# New subroutine "store_section_in_tree" extracted - Mon Jul  4 13:35:50 2011.
-#
-sub store_section_in_tree {
-    my $self  = shift;
-    my $node  = shift;
-    my $check = shift;
-    my $key   = shift;
-    my $v     = shift;
-
-    return unless defined $v;
-
-    $logger->info( "reading key '$key' from Patch file (for node "
-          . $node->location
-          . ")" );
-    my $elt_name = $logger->debug("$key value: $v");
-    my $type     = $node->element_type($key);
-    my $elt_obj  = $node->fetch_element( name => $key, check => $check );
-    $v =~ s/^\s*\n//;
-    chomp $v;
-
-    if ( $type eq 'list' ) {
-        my @v = split /[\s\n]*,[\s\n]*/, $v;
-        chomp @v;
-        $logger->debug( "list $key store set '" . join( "','", @v ) . "'" );
-        $elt_obj->store_set( \@v, check => $check );
-    }
-    elsif ( my $found = $node->find_element( $key, case => 'any' ) ) {
-        my @elt = ($found);
-        my @v = ( $found eq 'Description' ) ? ( split /\n/, $v, 2 ) : ($v);
-        unshift @elt, 'Synopsis' if $found eq 'Description';
-        foreach (@elt) {
-            my $sub_v = shift @v;
-            $logger->debug("storing $_  value: $sub_v");
-            $node->fetch_element($_)->store( value => $sub_v, check => $check );
-        }
-    }
-    else {
-
-        # try anyway to trigger an error message
-        $node->fetch_element($key)->store( value => $v, check => $check );
-    }
 }
 
 sub write {
@@ -133,61 +92,41 @@ sub write {
     # file_path  => './my_test/etc/foo/foo.conf'
     # io_handle  => $io           # IO::File object
 
-    croak "Undefined file handle to write"
-      unless defined $args{io_handle};
+    # io_handle is not defined as no file is specified in model
 
-    my $node     = $args{object};
-    my $ioh      = $args{io_handle};
-    my @sections = [ $self->package_spec( $node->fetch_element('source') ) ];
+    my $patch_dir = $args{root} . $args{config_dir};
+    my $check     = $args{check};
+    my $node      = $args{object};
 
-    my $binary_hash = $node->fetch_element('binary');
-    foreach my $binary_name ( $binary_hash->get_all_indexes ) {
-        my $ref = [
-            Package => $binary_name,
-            $self->package_spec( $binary_hash->fetch_with_id($binary_name) )
-        ];
+    my $patch_name = $node->index_value;
 
-        push @sections, $ref;
-    }
+    my $patch_file = "$patch_dir/$patch_name";
+    $logger->info("Writing patch $patch_file");
+    my $io = IO::File->new($patch_file,'w')
+      || Config::Model::Exception::Syntax->throw(
+        message => "cannot write patch $patch_file" );
 
-    $self->write_dpkg_file( $ioh, \@sections, ",\n" );
-
-    return 1;
-}
-
-sub package_spec {
-    my ( $self, $node ) = @_;
-
-    my @section;
-    my $description_ref;
-    foreach my $elt ( $node->get_element_name ) {
-        my $type    = $node->element_type($elt);
-        my $elt_obj = $node->fetch_element($elt);
-
-        if ( $type eq 'hash' ) {
-            die "package_spec: unexpected hash type in "
-              . $node->name
-              . " element $elt\n";
+    foreach my $elt ( $node -> get_element_name ) {
+        my $v = $node->fetch_element_value($elt) ;
+        next unless defined $v and $v;
+        
+        if ($elt eq 'Synopsis') {
+            $v .= "\n" . $node->fetch_element_value('Description') ;
+            $io->print("Description:");
+            $self->write_dpkg_text($io,$v) ;
         }
-        elsif ( $type eq 'list' ) {
-            my @v = $elt_obj->fetch_all_values;
-            push @section, $elt, \@v if @v;
-        }
-        elsif ( $elt eq 'Synopsis' ) {
-            my $v = $node->fetch_element_value($elt);
-            push @section, 'Description', $v;    # mandatory field
-            $description_ref = \$section[$#section];
-        }
-        elsif ( $elt eq 'Description' ) {
-            $$description_ref .=
-              "\n" . $node->fetch_element_value($elt);    # mandatory field
+        elsif ($elt eq 'Description') { } # done in Synopsis
+        elsif ($elt eq 'diff' ) {
+            $io->print($node->fetch_element_value('diff')) ;
         }
         else {
-            my $v = $node->fetch_element_value($elt);
-            push @section, $elt, $v if $v;
+            $io->print("$elt:");
+            $self->write_dpkg_text($io,$v) ;
         }
+
     }
-    return @section;
+
+    return 1;
 }
 
 1;
