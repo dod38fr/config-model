4036b53815f9808eb80116f16ab29c13fc4198ea
Dominique Dumont <domi@komarr.gre.hp.com>
2011-08-29 17:49:25 +0200

    warn in case of duplicated dependencies

--- config-model-core/lib/Config/Model/models/Debian/Dpkg/Control/Source.pl
+++ config-model-core/lib/Config/Model/models/Debian/Dpkg/Control/Source.pl
@@ -83,7 +83,8 @@ The archive area and section for each package should be specified in the package
           'class' => 'Config::Model::Debian::Dependency',
           'type' => 'leaf'
         },
-        'type' => 'list'
+        'type' => 'list',
+        duplicates => 'warn',
       },
       'Build-Conflicts',
       {
