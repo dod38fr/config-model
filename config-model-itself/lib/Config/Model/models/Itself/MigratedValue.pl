36075e7a03e4a35470d5f8c5ba38651b9604edc6
Dominique Dumont <domi@komarr.gre.hp.com>
2011-10-10 09:40:50 +0200

    replaced value can be a string, not only an uniline

--- config-model-itself/lib/Config/Model/models/Itself/MigratedValue.pl
+++ config-model-itself/lib/Config/Model/models/Itself/MigratedValue.pl
@@ -1,20 +1,3 @@
-#    Copyright (c) 2007-2011 Dominique Dumont.
-#
-#    This file is part of Config-Model-Itself.
-#
-#    Config-Model-Itself is free software; you can redistribute it
-#    and/or modify it under the terms of the GNU Lesser Public License
-#    as published by the Free Software Foundation; either version 2.1
-#    of the License, or (at your option) any later version.
-#
-#    Config-Model-Itself is distributed in the hope that it will be
-#    useful, but WITHOUT ANY WARRANTY; without even the implied
-#    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-#    See the GNU Lesser Public License for more details.
-#
-#    You should have received a copy of the GNU Lesser Public License
-#    along with Config-Model-Itself; if not, write to the Free Software
-#    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
 
 [
   [
@@ -42,7 +25,7 @@
        => {
 	   type => 'hash',
 	   index_type => 'string' ,
-	   cargo => { type => 'leaf', value_type => 'uniline' } ,
+	   cargo => { type => 'leaf', value_type => 'string' } ,
 	   description => 'Sometime, using the value of a tree leaf is not enough and you need to substitute a replacement for any value you can get. This replacement can be done using a hash like notation within the formula using the %replace hash. Example $replace{$who} , where "who => \'- who_elt\' ',
 	  },
 
