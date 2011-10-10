dec191f7352ea5ea6171a14bee9634c46f08ca33
Dominique Dumont <domi@komarr.gre.hp.com>
2011-10-10 09:28:17 +0200

    updated license wording

--- config-model-tk-ui/lib/Config/Model/TkUI.pm
+++ config-model-tk-ui/lib/Config/Model/TkUI.pm
@@ -1249,7 +1249,7 @@ Dominique Dumont, (ddumont at cpan dot org)
     This file is part of Config-Model.
 
     Config-Model is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Lesser Public License as
+    modify it under the terms of the GNU Lesser General Public License as
     published by the Free Software Foundation; either version 2.1 of
     the License, or (at your option) any later version.
 
@@ -1258,7 +1258,7 @@ Dominique Dumont, (ddumont at cpan dot org)
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
     Lesser Public License for more details.
 
-    You should have received a copy of the GNU Lesser Public License
+    You should have received a copy of the GNU Lesser General Public License
     along with Config-Model; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
     02110-1301 USA
