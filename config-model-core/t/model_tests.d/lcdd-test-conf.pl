cff45267f0bab1674b7e8605e7ede25047c7cb2d
Dominique Dumont <domi@komarr.gre.hp.com>
2011-10-16 12:14:15 +0200

    fixed test to work even if lcdproc is not installed

--- config-model-core/t/model_tests.d/lcdd-test-conf.pl
+++ config-model-core/t/model_tests.d/lcdd-test-conf.pl
@@ -3,6 +3,15 @@ $conf_file_name = "LCDd.conf" ;
 $conf_dir = "etc" ;
 $model_to_test = "LCDd" ;
 
+my @fix_warnings ;
+
+push @fix_warnings,
+    ( 
+        load_warnings => [ qr/code check returned false/ ],
+        load => "server DriverPath=/tmp/" , # just a work-around
+    ) 
+    unless -d '/usr/lib/lcdproc/' ;
+
 @tests = (
     { # t0
      check => { 
@@ -10,9 +19,10 @@ $model_to_test = "LCDd" ;
        'server Hello:1',           qq("   LCDproc et Config::Model!") ,
        'server GoodBye:0',           qq!"    GoodBye"! ,
        'server GoodBye:1',           qq("    LCDproc!") ,
-        'server Driver', 'curses',
+       'server Driver', 'curses',
        'curses Size', '20x2',
      },
+     @fix_warnings ,
      errors => [ 
             # qr/value 2 > max limit 0/ => 'fs:"/var/chroot/lenny-i386/dev" fs_passno=0' ,
         ],
