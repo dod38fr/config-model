539f38ae6d65e2ff68faa14cb187c8b36cf1aef7
Dominique Dumont <domi@komarr.gre.hp.com>
2011-11-08 18:28:08 +0100

    first multistrap version

---
+++ config-model-core/lib/Config/Model/models/Multistrap.pl
@@ -0,0 +1,99 @@
+[
+  {
+    'accept' => [
+      '\\w+',
+      {
+        'value_type' => 'uniline',
+        'warn' => 'Handling unknown parameter as unlinie value.',
+        'type' => 'leaf'
+      }
+    ],
+    'read_config' => [
+      {
+        'auto_create' => '1',
+        'section_map' => {
+          'General' => '!'
+        },
+        'backend' => 'ini_file',
+        'split_list_value' => '\\s+',
+        'store_class_in_hash' => 'sections'
+      }
+    ],
+    'name' => 'Multistrap',
+    'element' => [
+      'include',
+      {
+        'value_type' => 'uniline',
+        'class' => 'Config::Model::Value::PresetFromInclude',
+        'type' => 'leaf'
+      },
+      'arch',
+      {
+        'value_type' => 'enum',
+        'type' => 'leaf',
+        'choice' => [
+          'alpha',
+          'arm',
+          'armel',
+          'powerpc'
+        ]
+      },
+      'directory',
+      {
+        'value_type' => 'uniline',
+        'type' => 'leaf'
+      },
+      'aptsources',
+      {
+        'cargo' => {
+          'value_type' => 'reference',
+          'type' => 'leaf',
+          'refer_to' => '- sections'
+        },
+        'duplicates' => 'forbid',
+        'type' => 'list'
+      },
+      'bootstrap',
+      {
+        'cargo' => {
+          'value_type' => 'reference',
+          'type' => 'leaf',
+          'refer_to' => '- sections'
+        },
+        'duplicates' => 'forbid',
+        'type' => 'list'
+      },
+      'omitrequired',
+      {
+        'value_type' => 'boolean',
+        'type' => 'leaf'
+      },
+      'addimportant',
+      {
+        'value_type' => 'boolean',
+        'type' => 'leaf'
+      },
+      'configscript',
+      {
+        'value_type' => 'uniline',
+        'type' => 'leaf'
+      },
+      'setupscript',
+      {
+        'value_type' => 'uniline',
+        'type' => 'leaf'
+      },
+      'sections',
+      {
+        'cargo' => {
+          'type' => 'node',
+          'config_class_name' => 'Multistrap::Section'
+        },
+        'type' => 'hash',
+        'index_type' => 'string'
+      }
+    ]
+  }
+]
+;
+
---
+++ config-model-core/lib/Config/Model/models/Multistrap/Section.pl
@@ -0,0 +1,40 @@
+[
+  {
+    'accept' => [
+      '\\w+',
+      {
+        'value_type' => 'uniline',
+        'warn' => 'Handling unknown parameter as unlinie value.',
+        'type' => 'leaf'
+      }
+    ],
+    'name' => 'Multistrap::Section',
+    'element' => [
+      'packages',
+      {
+        'cargo' => {
+          'value_type' => 'uniline',
+          'type' => 'leaf'
+        },
+        'type' => 'list'
+      },
+      'source',
+      {
+        'value_type' => 'uniline',
+        'type' => 'leaf'
+      },
+      'keyring',
+      {
+        'value_type' => 'uniline',
+        'type' => 'leaf'
+      },
+      'suite',
+      {
+        'value_type' => 'uniline',
+        'type' => 'leaf'
+      }
+    ]
+  }
+]
+;
+
