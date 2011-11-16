6ceb842176c8d0a5cd88387118c5804ddfa49fe5
Dominique Dumont <domi@komarr.gre.hp.com>
2011-11-16 18:49:51 +0100

    added missing parameters and doc

--- config-model-core/lib/Config/Model/models/Multistrap.pl
+++ config-model-core/lib/Config/Model/models/Multistrap.pl
@@ -16,6 +16,10 @@
           'General' => '!'
         },
         'backend' => 'ini_file',
+        'write_boolean_as' => [
+          'false',
+          'true'
+        ],
         'split_list_value' => '\\s+',
         'store_class_in_hash' => 'sections'
       }
@@ -52,9 +56,21 @@
           'refer_to' => '- sections'
         },
         'duplicates' => 'forbid',
-        'type' => 'list'
+        'type' => 'list',
+        'description' => 'aptsources is a list of sections to be used in the /etc/apt/sources.list.d/multistrap.sources.list of the target. Order is not important.'
       },
       'bootstrap',
+      {
+        'cargo' => {
+          'value_type' => 'reference',
+          'type' => 'leaf',
+          'refer_to' => '- sections'
+        },
+        'duplicates' => 'forbid',
+        'type' => 'list',
+        'description' => 'the bootstrap option determines which repository is used to calculate the list of Priority: required packages and which packages go into the rootfs. The order of sections is not important.'
+      },
+      'debootstrap',
       {
         'cargo' => {
           'value_type' => 'reference',
@@ -82,6 +98,32 @@
       'setupscript',
       {
         'value_type' => 'uniline',
+        'type' => 'leaf'
+      },
+      'cleanup',
+      {
+        'value_type' => 'boolean',
+        'type' => 'leaf',
+        'description' => 'remove apt cache data, downloaded Packages files and the apt package cache.'
+      },
+      'noauth',
+      {
+        'value_type' => 'boolean',
+        'type' => 'leaf',
+        'description' => 'allow the use of unauthenticated repositories'
+      },
+      'explicitsuite',
+      {
+        'value_type' => 'boolean',
+        'upstream_default' => '0',
+        'type' => 'leaf',
+        'description' => 'whether to add the /suite to be explicit about where apt needs to look for packages.'
+      },
+      'unpack',
+      {
+        'value_type' => 'boolean',
+        'summary' => 'extract all downloaded archives',
+        'upstream_default' => '1',
         'type' => 'leaf'
       },
       'sections',
