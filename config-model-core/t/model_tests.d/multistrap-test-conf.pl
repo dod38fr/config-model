2dbf459a87a9a24ef7a7cad4139065bb1dec03d3
Dominique Dumont <domi@komarr.gre.hp.com>
2011-11-17 17:42:48 +0100

    test for multistrap from scratch

--- config-model-core/t/model_tests.d/multistrap-test-conf.pl
+++ config-model-core/t/model_tests.d/multistrap-test-conf.pl
@@ -10,6 +10,16 @@ $model_to_test = "Multistrap";
                 'sections:Toolchains packages:1', 'linux-libc-dev-arm-cross',
             },
     },
+    {
+        name => 'from_scratch',
+        config_file => '/home/foo/my_arm.conf',
+        load => "include=/usr/share/multistrap/arm.conf" ,
+        file_check_sub => sub { 
+            my $r = shift ; 
+            # this file was created after the load instructions above
+            unshift @$r, "/home/foo/my_arm.conf";
+        }
+    }
 );
 
 1;
