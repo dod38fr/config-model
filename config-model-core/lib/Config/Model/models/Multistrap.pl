b5e350ee92e739f98b541f5408d495a2ff8c446a
Dominique Dumont <domi@komarr.gre.hp.com>
2011-12-19 18:46:29 +0100

    added some doc

--- config-model-core/lib/Config/Model/models/Multistrap.pl
+++ config-model-core/lib/Config/Model/models/Multistrap.pl
@@ -33,8 +33,10 @@
       {
         'convert' => 'lc',
         'value_type' => 'uniline',
+        'summary' => 'Include file for cascaded configuration',
         'class' => 'Config::Model::Value::LayeredInclude',
-        'type' => 'leaf'
+        'type' => 'leaf',
+        'description' => 'To support multiple variants of a basic (common) configuration, "multistrap" allows configuration files to include other (more general) configuration files. i.e. the most detailed / specific configuration file is specified on the command line and that file includes another file which is shared by other configurations.'
       },
       'arch',
       {
@@ -50,7 +52,9 @@
       'directory',
       {
         'value_type' => 'uniline',
-        'type' => 'leaf'
+        'summary' => 'target directory',
+        'type' => 'leaf',
+        'description' => 'top level directory where the bootstrap will be created'
       },
       'aptsources',
       {
@@ -84,8 +88,10 @@
           'type' => 'leaf',
           'refer_to' => '- sections'
         },
+        'status' => 'deprecated',
         'duplicates' => 'forbid',
-        'type' => 'list'
+        'type' => 'list',
+        'description' => 'Replaced by bootstrap parameter'
       },
       'omitrequired',
       {
