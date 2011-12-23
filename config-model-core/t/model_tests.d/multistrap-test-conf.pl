a6cc4f5e260cb1207dfd64c357b6746c1e1ef882
Dominique Dumont <domi@komarr.gre.hp.com>
2011-12-23 14:26:48 +0100

    improved multistrap tests

--- config-model-core/t/model_tests.d/multistrap-test-conf.pl
+++ config-model-core/t/model_tests.d/multistrap-test-conf.pl
@@ -1,6 +1,16 @@
 
 $model_to_test = "Multistrap";
 
+$from_scratch_file = <<'EOF' ,
+## This file was written by Config::Model
+## You may modify the content of this file. Configuration 
+## modifications will be preserved. Modifications in
+## comments may be mangled.
+
+[general]
+include=/usr/share/multistrap/crosschroot.conf
+EOF
+
 @tests = (
     {
         name        => 'arm',
@@ -13,11 +23,24 @@ $model_to_test = "Multistrap";
     {
         name => 'from_scratch',
         config_file => '/home/foo/my_arm.conf',
-        load => "include=/usr/share/multistrap/arm.conf" ,
+        load => "include=/usr/share/multistrap/crosschroot.conf" ,
+
+        # values brought by included file
+        check_layered  => {
+                'sections:debian packages:0' ,'dpkg-dev',
+                'sections:base packages:0', 'gcc-4.2-base',
+            },
+        check  => {
+                'sections:toolchains packages:0' ,undef,
+                'sections:toolchains packages:1', undef,
+            },
         file_check_sub => sub { 
             my $r = shift ; 
             # this file was created after the load instructions above
             unshift @$r, "/home/foo/my_arm.conf";
+        },
+        file_content => { 
+            "/home/foo/my_arm.conf" => $from_scratch_file ,
         }
     },
     {
