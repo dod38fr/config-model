3b2378c9c123ce5687fdac9296d9c67882af8e7c
Dominique Dumont <domi@komarr.gre.hp.com>
2012-01-28 08:21:25 +0100

    cleanup

--- config-model-core/lib/Config/Model/Backend/Any.pm
+++ config-model-core/lib/Config/Model/Backend/Any.pm
@@ -1,21 +1,3 @@
-#    Copyright (c) 2010 Dominique Dumont.
-#
-#    This file is part of Config-Model.
-#
-#    Config-Model is free software; you can redistribute it and/or
-#    modify it under the terms of the GNU Lesser Public License as
-#    published by the Free Software Foundation; either version 2.1 of
-#    the License, or (at your option) any later version.
-#
-#    Config-Model is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-#    Lesser Public License for more details.
-#
-#    You should have received a copy of the GNU Lesser Public License
-#    along with Config-Model; if not, write to the Free Software
-#    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
-
 package Config::Model::Backend::Any ;
 
 use Carp;
@@ -23,6 +5,7 @@ use strict;
 use warnings ;
 use Config::Model::Exception ;
 use Any::Moose ;
+use namespace::autoclean;
 
 use File::Path;
 use Log::Log4perl qw(get_logger :levels);
@@ -155,7 +138,6 @@ sub write_data_and_comments {
     return $res ;
 }
 
-no Any::Moose ;
 __PACKAGE__->meta->make_immutable ;
 
 1;
