a76e9a61c5f1f601b63d9141712504d4557aced9
Dominique Dumont <dod@debian.org>
2012-05-19 19:39:41 +0200

    test: check that AuthorizedKeysFile list is written on one line

--- config-model-openssh/t/model_tests.d/sshd-test-conf.pl
+++ config-model-openssh/t/model_tests.d/sshd-test-conf.pl
@@ -11,6 +11,9 @@ $conf_dir = '/etc/ssh' ;
             'AuthorizedKeysFile:0' => '/etc/ssh/userkeys/%u',
             'AuthorizedKeysFile:1' => '/var/lib/misc/userkeys2/%u',
         },
+        file_contents_like => {
+            '/etc/ssh/sshd_config' => qr!/etc/ssh/userkeys/%u /var/lib/misc/userkeys2/%u! ,
+        }
     },
 );
 
