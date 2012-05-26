82c52071e054c7c0bb5f1b90e3a7a01d5602565f
Dominique Dumont <dod@debian.org>
2012-05-26 11:59:33 +0200

    doc update

--- config-model-core/lib/Config/Model/Tester.pm
+++ config-model-core/lib/Config/Model/Tester.pm
@@ -485,7 +485,7 @@ data. Use C<dump_errors> if you expect issues:
 
 =item *
 
-Likewise, specify any expected warnings:
+Likewise, specify any expected warnings (note the list must contain only C<qr> stuff):
 
         dump_warnings => [ (qr/deprecated/) x 3 ],
 
