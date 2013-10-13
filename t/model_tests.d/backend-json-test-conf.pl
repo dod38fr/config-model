267db926d9e37a15c7da71f5aa06cd9dfbacd8fe
Dominique Dumont <dod@debian.org>
2013-10-13 13:42:39 +0200

    test JSON backend

---
+++ t/model_tests.d/backend-json-test-conf.pl
@@ -0,0 +1,52 @@
+use Config::Model::BackendMgr;
+
+# test shellvar backend
+$conf_dir = '/etc';
+$conf_file_name = 'hosts.json';
+
+$model->create_config_class(
+    name => 'Host',
+
+    element => [
+        [qw/ipaddr canonical alias/] => {
+            type       => 'leaf',
+            value_type => 'uniline',
+        },
+        dummy => {qw/type leaf value_type uniline default toto/},
+    ]
+);
+$model->create_config_class(
+    name => 'Hosts',
+
+    read_config => [
+        {
+            backend     => 'json',
+            config_dir  => '/etc/',
+            file        => 'hosts.json',
+        },
+    ],
+
+    element => [
+        record => {
+            type  => 'list',
+            cargo => {
+                type              => 'node',
+                config_class_name => 'Host',
+            },
+        },
+    ]
+);
+
+$model_to_test = "Hosts";
+
+@tests = (
+    {
+        name  => 'basic',
+        check => [
+            'record:0 ipaddr' => '127.0.0.1',
+            'record:1 canonical' => 'bilbo'
+        ]
+    },
+);
+
+1;
