8fcce4f0f314d9bf971d66f4340ba113435d78ee
Dominique Dumont <dod@debian.org>
2016-01-10 15:55:44 +0100

    WarpMaster: can store hash values...
    ... for check_list

--- lib/Config/Model/Role/WarpMaster.pm
+++ lib/Config/Model/Role/WarpMaster.pm
@@ -10,7 +10,7 @@ use Mouse::Util;
 use Log::Log4perl qw(get_logger :levels);
 use Scalar::Util qw/weaken/;
 
-my $logger = get_logger("WarpMaster"); # Warper ?
+my $logger = get_logger("Warper");
 
 has 'warp_these_objects' => (
     traits  => ['Array'],
@@ -18,32 +18,25 @@ has 'warp_these_objects' => (
     isa     => 'ArrayRef',
     default => sub { [] },
     handles => {
-        _slave_info    => 'elements',
-        _add_slave_info     => 'push',
-#        map_options    => 'map',
-#        filter_options => 'grep',
+        _slave_info        => 'elements',
+        _add_slave_info    => 'push',
+        _delete_slave       => 'delete',
+        has_warped_slaves  => 'count',
         # find_slave_idx    => 'first_index', not available in Mouse
-        delete_slave => 'delete',
-#        get_option     => 'get',
-#        join_options   => 'join',
-#        count_options  => 'count',
-        has_warped_slaves    => 'count',
-#        has_no_options => 'is_empty',
-#        sorted_options => 'sort',
     },
 );
 
-# Now I'm a warper !
 sub register {
-    my ( $self, $warped, $w_idx ) = @_;
+    my ( $self, $warped, $warper_name ) = @_;
 
     my $w_name = $warped->name;
-    $logger->debug( "Value: " . $self->name, " registered $w_name ($w_idx)" );
+    $logger->debug( $self->get_type . ": " . $self->name, " registered $w_name ($warper_name)" )
+        if $logger->is_debug;
 
     # weaken only applies to the passed reference, and there's no way
     # to duplicate a weak ref. Only a strong ref is created. See
     #  qw(weaken) module for weaken()
-    my @tmp = ( $warped, $w_name, $w_idx );
+    my @tmp = ( $warped, $w_name, $warper_name );
     weaken( $tmp[0] );
     $self->_add_slave_info( \@tmp );
 
@@ -52,7 +45,8 @@ sub register {
 
 sub unregister {
     my ( $self, $w_name ) = @_;
-    $logger->debug( "Value: " . $self->name, " unregister $w_name" );
+    $logger->debug(  $self->get_type .": " . $self->name, " unregister $w_name" )
+        if $logger->is_debug;
 
     my $idx = 0;
     foreach my $info ($self->_slave_info) {
@@ -60,15 +54,14 @@ sub unregister {
         $idx++;
     }
 
-    $self->delete_slave($idx);
+    $self->_delete_slave($idx);
 }
 
 # And I'm going to warp them ...
 sub trigger_warp {
     my $self = shift;
-
-    # retrieve current value if not provided
-    my $value = @_ ? $_[0] : $self->fetch_no_check;
+    my $value = shift;
+    my $str_val = shift // $value // 'undefined';
 
     foreach my $ref ( $self->_slave_info ) {
         my ( $warped, $w_name, $warp_index ) = @$ref;
@@ -76,9 +69,8 @@ sub trigger_warp {
 
         # pure warp of object
         if ($logger->is_debug) {
-            my $str = $value // 'undefined';
-            $logger->debug("trigger_warp: from ", $self->name,
-                           " (value $str) warping '$w_name'");
+            $logger->debug("trigger_warp: ".$self->get_type." ", $self->name,
+                           " warps '$w_name' with value <$str_val> ");
         }
         $warped->trigger( $value, $warp_index );
     }
@@ -87,7 +79,7 @@ sub trigger_warp {
 sub get_warped_slaves {
     my $self = shift;
 
-    # needs to clean up weak ref to object that were destroyed
+    # grep is used to clean up weak ref to object that were destroyed
     return grep { defined $_ } map { $_->[0] } $self->_slave_info;
 }
 
