61363361715633e003b533fa8158d411998aedac
Dominique Dumont <dod@debian.org>
2016-03-09 18:49:19 +0100

    Lister: added _file info in appli_info
    to compose better error messages.

--- lib/Config/Model/Lister.pm
+++ lib/Config/Model/Lister.pm
@@ -27,6 +27,7 @@ sub available_models {
             next if $file =~ m!/README!;
             next if $file =~ /(~|\.bak|\.orig)$/;
             my ($appli) = ( $file =~ m!.*/([\w\-]+)! );
+            $appli_info{$appli}{_file} = $file;
             open( F, $file ) || die "Can't open file $file:$!";
             while (<F>) {
                 chomp;
