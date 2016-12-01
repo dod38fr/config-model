50d9310f46a3e2f7fc6bc4ae9954666a1c762954
Dominique Dumont <dod@debian.org>
2016-12-01 21:26:06 +0100

    test init error with weird backend setup
    test a condition where a failure occurs when:
    - master contains nodes
    - each node has a backend
    - an element of a node has a constraint
    - the file contains an element that does not fir the constraint
    
    loading such a setup was impossible even with check => no.
    
    This has been fixed in a previous commit, but testing this condition was
    much harder

---
+++ t/model_tests.d/multi-ini-test-conf.pl
@@ -0,0 +1,62 @@
+
+# test inifile backend with multiple ini files
+
+# specify the name of the class to test
+$model_to_test = "MultiMiniIni";
+
+# create minimal model to test ini file backend.
+
+# this class is used by MultiMiniIni class below
+$model->create_config_class(
+    name    => 'MultiIniTest::Class',
+    element => [
+        int_with_max => {qw/type leaf value_type integer max 10/},
+    ],
+    read_config => [{
+        backend     => 'IniFile',
+        config_dir  => '/etc/',
+        file        => '&index.conf',
+        auto_create => 1,
+    }],
+);
+
+$model->create_config_class(
+    name => 'MultiMiniIni',
+    element => [
+        service => {
+            type  => 'hash',
+            index_type => 'string',
+            # require to trigger load of bar.conf
+            default_keys => ['bar'],
+            cargo => {
+                type       => 'node',
+                config_class_name => 'MultiIniTest::Class'
+            }
+        },
+    ],
+    read_config => [{
+        backend     => 'Yaml',
+        config_dir  => '/etc/',
+        file        => 'service.yml',
+        auto_create => 1,
+    }],
+);
+
+
+# the test suite
+@tests = (
+    {
+        name  => 'max-overflow',
+        # work only with Config::Model > 2.094 because of an obscure
+        # initialisation bug occuring while loading a bad value in
+        # a sub-node (thanks systemd)
+        load => 'service:bar int_with_max=9',
+        file_check_sub => sub {
+            my $list_ref = shift ;
+            # file added because of default bar key
+            push @$list_ref, "/etc/service.yml" ;
+        },
+    },
+);
+
+1;
