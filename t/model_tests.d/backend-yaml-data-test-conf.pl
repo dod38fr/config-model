74a77481d42f3f56a469bcf10ff21f95dfb21dc8
Dominique Dumont <dod@debian.org>
2017-05-14 20:49:01 +0200

    test yaml_class parameter

---
+++ t/model_tests.d/backend-yaml-data-test-conf.pl
@@ -0,0 +1,40 @@
+use Config::Model::BackendMgr;
+
+$conf_dir = '/etc';
+$conf_file_name = 'test.yaml';
+
+$model->create_config_class(
+    name => 'Master',
+
+    read_config => [
+        {
+            backend     => 'yaml',
+            config_dir  => '/etc/',
+            file        => 'test.yaml',
+            # as of 2017, YAML is the only parser that writes back boolean value as unquoted true/false
+            yaml_class  => 'YAML',
+        },
+    ],
+
+    element => [
+        true_bool  => { qw/type leaf value_type boolean/, write_as => [qw/false true/]},
+        false_bool => { qw/type leaf value_type boolean/, write_as => [qw/false true/]},
+        null_value => { qw/type leaf value_type uniline/},
+    ]
+);
+
+$model_to_test = "Master";
+
+@tests = (
+    {
+        name  => 'basic',
+        check => [
+            # values are translated from whatever YAML lib returns to true and false
+            'true_bool' => 'true',
+            'false_bool' => 'false',
+            null_value => undef
+        ]
+    },
+);
+
+1;
