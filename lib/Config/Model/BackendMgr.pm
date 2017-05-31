0511498f83a4ee371a02bd1d7c16bd507cdb26f4
Dominique Dumont <dod@debian.org>
2017-05-31 19:37:12 +0200

    deprecate custom backend

--- lib/Config/Model/BackendMgr.pm
+++ lib/Config/Model/BackendMgr.pm
@@ -374,6 +374,7 @@ sub try_read_backend {
     my ( $res, $file_path, $error );
 
     if ( $backend eq 'custom' ) {
+        $logger->warn("custom read/write backend is deprecated. Please replace with a backend inheriting Config::Model::Backend::Any");
         my $c = my $file = delete $read->{class};
         $file =~ s!::!/!g;
         my $f = delete $read->{function} || 'read';
