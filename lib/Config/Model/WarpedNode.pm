ea552440bb19c4f88e974b0c54220ca0751799f0
Dominique Dumont <dod@debian.org>
2017-11-23 19:06:33 +0100

    WarpedNode: forward fetch_gist instead of gist
    fetch_gist is the method to retrieve the computed gist. gist returns
    only the expression stored in the model.

--- lib/Config/Model/WarpedNode.pm
+++ lib/Config/Model/WarpedNode.pm
@@ -71,7 +71,7 @@ sub config_model {
 # Forward selected methods (See man perltootc)
 foreach my $method (
     qw/fetch_element config_class_name copy_from get_element_name
-    gist has_element is_element_available element_type load
+    fetch_gist has_element is_element_available element_type load
     fetch_element_value get_type get_cargo_type dump_tree needs_save
     describe get_help get_help_as_text children get set accept_regexp/
     ) {
