4b4494919cc5c60e45503681e66750f2cb74a8c1
Dominique Dumont <dod@debian.org>
2018-12-14 19:43:49 +0100

    PBP: use plain return

--- lib/Config/Model/Value.pm
+++ lib/Config/Model/Value.pm
@@ -342,14 +342,14 @@ sub set_migrate_from {
 sub migrate_value {
     my $self = shift;
 
-    return undef if $self->{migration_done};
-    return undef if $self->instance->initial_load;
+    return if $self->{migration_done};
+    return if $self->instance->initial_load;
     $self->{migration_done} = 1;
 
     # avoid warning when reading deprecated values
     my $result = $self->{_migrate_from}->compute( check => 'skip' );
 
-    return undef unless defined $result;
+    return unless defined $result;
 
     # check if the migrated result fits with the constraints of the
     # Value object
@@ -1749,7 +1749,7 @@ sub fetch {
             warn "Warning: fetch [".$self->name,"] skipping value $str because of the following errors:\n$msg\n\n"
                 if not $silent and $msg;
         }
-        return undef;
+        return;
     }
 
     Config::Model::Exception::WrongValue->throw(
