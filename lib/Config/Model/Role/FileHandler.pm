0df1009fc338447e605e60c5ba1d6239719bf546
Dominique Dumont <dod@debian.org>
2019-07-27 19:54:50 +0200

    also tweak ~ dir for tests in type coercion
    I.e. fix systemd test that broke with previous Config::Model version

--- lib/Config/Model/Role/FileHandler.pm
+++ lib/Config/Model/Role/FileHandler.pm
@@ -18,8 +18,9 @@ use Config::Model::TypeConstraints;
 my $logger = get_logger("FileHandler");
 
 # used only for tests
-my $__test_home = '';
-sub _set_test_home { $__test_home = shift; }
+sub _set_test_home {
+    Config::Model::TypeConstraints::_set_test_home(shift) ;
+}
 
 # Configuration directory where to read and write files. This value
 # does not override the configuration directory specified in the model
@@ -35,7 +36,7 @@ sub get_tuned_config_dir {
         # TODO: should this be my_config ? May be once this is done:
         # https://github.com/perl5-utils/File-HomeDir/pull/5/files
         # beware of compat and migration issues
-        my $home = $__test_home || File::HomeDir->my_home;
+        my $home =  &Config::Model::TypeConstraints::_get_test_home || File::HomeDir->my_home;
         $dir =~ s/^~/$home/;
     }
 
--- lib/Config/Model/TypeConstraints.pm
+++ lib/Config/Model/TypeConstraints.pm
@@ -3,8 +3,23 @@ package Config::Model::TypeConstraints;
 use Mouse;
 use Mouse::Util::TypeConstraints;
 
+# used only for tests
+my $__test_home = '';
+sub _set_test_home { $__test_home = shift; }
+sub _get_test_home { return $__test_home ; }
+
 subtype 'Config::Model::TypeContraints::Path' => as 'Maybe[Path::Tiny]' ;
-coerce 'Config::Model::TypeContraints::Path' => from 'Str' => via sub { defined $_ ?  Path::Tiny::path($_) : undef ; } ;
+coerce 'Config::Model::TypeContraints::Path' => from 'Str' => via sub {
+    if (defined $_ and /^~/) {
+        # because of tests, we can't rely on Path::Tiny's tilde processing
+        # TODO: should this be my_config ? May be once this is done:
+        # https://github.com/perl5-utils/File-HomeDir/pull/5/files
+        # beware of compat and migration issues
+        my $home = $__test_home || File::HomeDir->my_home;
+        s/^~/$home/;
+    }
+    return defined $_ ?  Path::Tiny::path($_) : undef ;
+} ;
 
 1;
 
