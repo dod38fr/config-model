99108fe8ad58ad1e3ab96f98a7278a10205cba64
Dominique Dumont <dod@debian.org>
2021-11-13 12:05:50 +0100

    refactor (ListId): remove $::_use_log4perl_to_warn var
    Used for ease a change that broke all warning tests

--- lib/Config/Model/ListId.pm
+++ lib/Config/Model/ListId.pm
@@ -41,15 +41,10 @@ sub BUILD {
     }
 
     if ( defined $self->{migrate_keys_from} ) {
-        if ($::_use_log4perl_to_warn) {
-            $user_logger->warn(
-                $self->name, "Using migrate_keys_from with ",
-                "list element is deprecated. Use migrate_values_from");
-        }
-        else {
-            warn $self->name, "Using migrate_keys_from with list element is deprecated.",
-                " Use migrate_values_from\n";
-        }
+        $user_logger->warn(
+            $self->name, "Using migrate_keys_from with ",
+            "list element is deprecated. Use migrate_values_from"
+        );
     }
 
     # Supply the mandatory parameter
