e0f07cd6c1f8ccfd9f4a14be513130993ee5b912
Dominique Dumont <dod@debian.org>
2021-11-28 18:34:29 +0100

    update (doc): remove license section
    License is added by dzil

--- lib/Config/Model.pm
+++ lib/Config/Model.pm
@@ -2781,25 +2781,6 @@ Contributors to this project are listed in alphabetical order:
 
 Many thanks for your help
 
-=head1 LICENSE
-
-    Copyright (c) 2005-2016 Dominique Dumont.
-
-    Config-Model is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Lesser General Public License as
-    published by the Free Software Foundation; either version 2.1 of
-    the License, or (at your option) any later version.
-
-    Config-Model is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Lesser Public License for more details.
-
-    You should have received a copy of the GNU Lesser General Public License
-    along with Config-Model; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
-    02110-1301 USA
-
 =head1 SEE ALSO
 
 L<Config::Model::Instance>,
