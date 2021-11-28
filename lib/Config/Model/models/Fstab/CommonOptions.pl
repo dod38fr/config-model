1be97b642302f56279d4ce4f86aeec4e33e45f95
Dominique Dumont <dod@debian.org>
2021-11-28 18:47:11 +0100

    feat (fstab model): add description to common options

--- lib/Config/Model/models/Fstab/CommonOptions.pl
+++ lib/Config/Model/models/Fstab/CommonOptions.pl
@@ -13,46 +13,56 @@ return [
     'element' => [
       'async',
       {
+        'description' => 'All I/O to the filesystem should be done asynchronously. (See also the sync option.)
+',
         'type' => 'leaf',
         'value_type' => 'boolean'
       },
       'atime',
       {
+        'description' => 'Do not use the noatime feature, so the inode access time is controlled by kernel defaults. See also the descriptions of the relatime and strictatime mount options.',
         'type' => 'leaf',
         'value_type' => 'boolean'
       },
       'auto',
       {
+        'description' => 'Can be mounted with the -a option of C<mount> command',
         'type' => 'leaf',
         'value_type' => 'boolean'
       },
       'dev',
       {
+        'description' => 'Interpret character or block special devices on the filesystem.',
         'type' => 'leaf',
         'value_type' => 'boolean'
       },
       'exec',
       {
+        'description' => 'Permit execution of binaries.',
         'type' => 'leaf',
         'value_type' => 'boolean'
       },
       'suid',
       {
+        'description' => 'Honor set-user-ID and set-group-ID bits or file capabilities when executing programs from this filesystem.',
         'type' => 'leaf',
         'value_type' => 'boolean'
       },
       'group',
       {
+        'description' => "Allow an ordinary user to mount the filesystem if one of that user\x{2019}s groups matches the group of the device. This option implies the options nosuid and nodev (unless overridden by subsequent options, as in the option line group,dev,suid).",
         'type' => 'leaf',
         'value_type' => 'boolean'
       },
       'mand',
       {
+        'description' => 'Allow mandatory locks on this filesystem. See L<fcntl(2)>.',
         'type' => 'leaf',
         'value_type' => 'boolean'
       },
       'user',
       {
+        'description' => 'Allow an ordinary user to mount the filesystem. The name of the mounting user is written to the mtab file (or to the private libmount file in /run/mount on systems without a regular mtab) so that this same user can unmount the filesystem again. This option implies the options noexec, nosuid, and nodev (unless overridden by subsequent options, as in the option line user,exec,dev,suid).',
         'help' => {
           '0' => 'Only root can mount the file system',
           '1' => 'user can mount the file system'
@@ -62,6 +72,9 @@ return [
       },
       'defaults',
       {
+        'description' => 'Use the default options: rw, suid, dev, exec, auto, nouser, and async.
+
+Note that the real set of all default mount options depends on the kernel and filesystem type. See the beginning of this section for more details.',
         'help' => {
           '1' => 'option equivalent to rw, suid, dev, exec, auto, nouser, and async'
         },
@@ -70,6 +83,7 @@ return [
       },
       'rw',
       {
+        'description' => 'Mount the filesystem read-write.',
         'help' => {
           '0' => 'read-only file system'
         },
@@ -78,6 +92,7 @@ return [
       },
       'relatime',
       {
+        'description' => "Update inode access times relative to modify or change time. Access time is only updated if the previous access time was earlier than the current modify or change time. (Similar to noatime, but it doesn\x{2019}t break mutt(1) or other applications that need to know if a file has been read since the last time it was modified.)",
         'type' => 'leaf',
         'value_type' => 'boolean'
       }
