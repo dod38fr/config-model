600d5b39854609ac38cdc87b9a3596cfb81f8254
Dominique Dumont <dod@debian.org>
2026-07-04 18:04:08 +0200

    Loader: move _split_string in Role::Utils
    Such a bag-of-tricks roles may not be a good idea...

--- lib/Config/Model/Loader.pm
+++ lib/Config/Model/Loader.pm
@@ -37,6 +37,8 @@ sub _build_instance ($self) {
     return $self->start_node->instance;
 }
 
+with 'Config::Model::Role::Utils';
+
 my %log_dispatch = (
     name => sub { my $loc = $_[0]->location; return $loc ? $_[0]->get_type." '$loc'" : "root node"},
     qs => sub { my $s = shift; unquote($s); return "'$s'"},
@@ -59,40 +61,6 @@ sub _log_cmd {
         : ref $cmd ? $$cmd : $cmd;
     $verbose_logger->info("command '$str': $message");
     return;
-}
-
-sub _split_string ($str) {
-    # do a split on ' ' but take quoted string into account
-    return (
-        $str =~ m/
-         (         # begin of *one* command
-          (?:        # group parts of a command (e.g ...:...=... )
-           (?:[^\s"']+)  # match anything but a space and a quote
-           |
-           (?:        # begin quoted group
-             "         # begin of a string
-              (?:        # begin group
-                \\"       # match an escaped quote
-                |         # or
-                [^"]      # anything but a quote
-              )*?        # lots of time
-             "         # end of the string
-           )          # end of quoted group
-           |          # match if I got more than one group
-           (?:        # begin quoted group
-             '         # begin of a string
-              (?:        # begin group
-                \\'       # match an escaped quote
-                |         # or
-                [^']      # anything but a quote
-              )*?        # lots of time
-             '         # end of the string
-           )          # end of quoted group
-                     # match if I got more than one group
-          )+      # can have several parts in one command
-         )        # end of *one* command
-        /gx    # 'g' means that all commands are fed into @command array
-    );         #"asdf ;
 }
 
 sub load ($self, %args) {
--- lib/Config/Model/Role/Utils.pm
+++ lib/Config/Model/Role/Utils.pm
@@ -15,6 +15,41 @@ sub _resolve_arg_shortcut ($args, @param_list) {
          :                           map { $_ => shift @$args; } @param_list;
 }
 
+sub _split_string ($str) {
+    # do a split on ' ' but take quoted string into account
+    return (
+        $str =~ m/
+         (         # begin of *one* command
+          (?:        # group parts of a command (e.g ...:...=... )
+           (?:[^\s"']+)  # match anything but a space and a quote
+           |
+           (?:        # begin quoted group
+             "         # begin of a string
+              (?:        # begin group
+                \\"       # match an escaped quote
+                |         # or
+                [^"]      # anything but a quote
+              )*?        # lots of time
+             "         # end of the string
+           )          # end of quoted group
+           |          # match if I got more than one group
+           (?:        # begin quoted group
+             '         # begin of a string
+              (?:        # begin group
+                \\'       # match an escaped quote
+                |         # or
+                [^']      # anything but a quote
+              )*?        # lots of time
+             '         # end of the string
+           )          # end of quoted group
+                     # match if I got more than one group
+          )+      # can have several parts in one command
+         )        # end of *one* command
+        /gx    # 'g' means that all commands are fed into @command array
+    );         #"asdf ;
+}
+
+
 1;
 
 
